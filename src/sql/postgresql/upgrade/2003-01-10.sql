26bf9a7d79eb398b0bdae084d2df6eeb9f941ff1
mrajkowski <mrajkowski@concursive.com>
2002-02-04 13:31:31 +0000

    no message

--- src/sql/postgresql/upgrade/2003-01-10.sql
+++ src/sql/postgresql/upgrade/2003-01-10.sql
@@ -3,4 +3,9 @@
 ALTER TABLE access ADD COLUMN alias INT DEFAULT -1;
 ALTER TABLE access ADD COLUMN assistant INT DEFAULT -1;
 
+ALTER TABLE access ALTER COLUMN alias SET DEFAULT -1;
+ALTER TABLE access ALTER COLUMN assistant SET DEFAULT -1;
+
+UPDATE access SET alias = -1;
+UPDATE access SET assistant = -1;
 
