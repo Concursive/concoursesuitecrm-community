a3892d8c1b024d5274e4ef97e7d44d82126ddbe0
chris <chris@concursive.com>
2002-02-12 21:25:53 +0000

    no message

--- src/sql/postgresql/upgrade/2003-01-10.sql
+++ src/sql/postgresql/upgrade/2003-01-10.sql
@@ -1,11 +1,1 @@
-/* 2-4-2002 Upgrade access table to support alias feature */
-
-ALTER TABLE access ADD COLUMN alias INT DEFAULT -1;
-ALTER TABLE access ADD COLUMN assistant INT DEFAULT -1;
-
-ALTER TABLE access ALTER COLUMN alias SET DEFAULT -1;
-ALTER TABLE access ALTER COLUMN assistant SET DEFAULT -1;
-
-UPDATE access SET alias = -1;
-UPDATE access SET assistant = -1;
-
+INSERT INTO system_modules (description) VALUES ('Contacts & Resources');
