7c7ae9bd170cc0cc5bf31c136b9e121cdd520024
chris <chris@concursive.com>
2002-02-13 21:53:08 +0000

    no message

--- src/sql/postgresql/upgrade/2003-01-10.sql
+++ src/sql/postgresql/upgrade/2003-01-10.sql
@@ -1,23 +1,3 @@
-INSERT INTO system_modules (description) VALUES ('Contacts & Resources');
-
-ALTER TABLE campaign ADD COLUMN reply_addr VARCHAR(255);
-ALTER TABLE campaign ADD COLUMN subject VARCHAR(255);
-ALTER TABLE campaign ADD COLUMN message TEXT;
-ALTER TABLE campaign ADD COLUMN send_method_id INT;
-
-ALTER TABLE campaign ALTER COLUMN reply_addr SET DEFAULT null;
-ALTER TABLE campaign ALTER COLUMN subject SET DEFAULT null;
-ALTER TABLE campaign ALTER COLUMN message SET DEFAULT null;
-ALTER TABLE campaign ALTER COLUMN send_method_id SET DEFAULT -1;
-
-UPDATE campaign SET send_method_id = -1;
-
-ALTER TABLE message ADD COLUMN subject VARCHAR(255);
-ALTER TABLE message ALTER COLUMN subject SET DEFAULT null;
-
-
-CREATE TABLE excluded_recipient (
-  id serial PRIMARY KEY,
-  campaign_id INT NOT NULL DEFAULT -1,
-  contact_id INT NOT NULL
-);
+alter table organization add column contract_end date default null;
+alter table organization add column alertdate date default null;
+alter table organization add column alert varchar(100) default null;
