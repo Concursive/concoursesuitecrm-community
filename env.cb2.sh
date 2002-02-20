62463944dbfcb7959a153b8fa53bf28e13d04bfb
mrajkowski <mrajkowski@concursive.com>
2002-02-20 21:29:18 +0000

    *** empty log message ***

--- env.cb2.sh
+++ env.cb2.sh
@@ -4,11 +4,4 @@
 #
 # Common shell environment settings
 #
-export CFS_HOME=/usr/local/cfs2
-export JAVA_HOME=/usr/local/java/jdk1.3.1_02
-export APACHE_HOME=/etc/httpd
-export TOMCAT_HOME=/usr/local/java/jakarta-tomcat-3.2.3
-export ANT_HOME=/usr/local/java/jakarta-ant-1.4
-
-export PATH=$PATH:${JAVA_HOME}/bin:${ANT_HOME}/bin
-export CLASSPATH=.:${TOMCAT_HOME}/lib/servlet.jar:${TOMCAT_HOME}/lib/mail.jar:${TOMCAT_HOME}/lib/xerces.jar:${TOMCAT_HOME}/lib/activation.jar:${TOMCAT_HOME}/lib/jdbc7.1-1.3.jar:${TOMCAT_HOME}/lib/mail.jar:${TOMCAT_HOME}/lib/mailapi.jar:${TOMCAT_HOME}/lib/smtp.jar:/home/ds21/html/WEB-INF/lib/darkhorseventures.jar:/home/ds21/html/WEB-INF/lib/theseus.jar
+export CLASSPATH=.:${TOMCAT_HOME}/common/lib/servlet.jar:${TOMCAT_HOME}/common/lib/mail.jar:${TOMCAT_HOME}/common/lib/activation.jar:${TOMCAT_HOME}/common/lib/pgjdbc2.jar:/home/cfs/html/WEB-INF/lib/darkhorseventures.jar:/home/cfs/html/WEB-INF/lib/theseus.jar
