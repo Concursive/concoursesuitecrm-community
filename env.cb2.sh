3622cc984c7907c4cb7e7bef260bfcf9ac2cf23d
mrajkowski <mrajkowski@concursive.com>
2002-03-13 20:12:43 +0000

    *** empty log message ***

--- env.cb2.sh
+++ env.cb2.sh
@@ -4,4 +4,9 @@
 #
 # Common shell environment settings
 #
+export TOMCAT_HOME=/usr/local/java/jakarta-tomcat-4.0.2-LE-jdk14
+export JAVA_HOME=/usr/local/java/j2sdk1.4.0
+export ANT_HOME=/usr/local/java/jakarta-ant-1.4.1
+PATH=$PATH:${JAVA_HOME}/bin:${ANT_HOME}/bin
+
 export CLASSPATH=.:${TOMCAT_HOME}/common/lib/servlet.jar:${TOMCAT_HOME}/common/lib/mail.jar:${TOMCAT_HOME}/common/lib/activation.jar:${TOMCAT_HOME}/common/lib/pgjdbc2.jar:/home/cfs/html/WEB-INF/lib/darkhorseventures.jar:/home/cfs/html/WEB-INF/lib/theseus.jar:/home/cfs/html/WEB-INF/lib/zeroio-iteam.jar
