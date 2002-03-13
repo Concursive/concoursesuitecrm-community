105a88f9b388ff977fc1bfb9b0644901e07c9b39
mrajkowski <mrajkowski@concursive.com>
2002-03-13 18:44:00 +0000

    *** empty log message ***

--- env.cb2.sh
+++ env.cb2.sh
@@ -4,4 +4,4 @@
 #
 # Common shell environment settings
 #
-export CLASSPATH=.:${TOMCAT_HOME}/common/lib/servlet.jar:${TOMCAT_HOME}/common/lib/mail.jar:${TOMCAT_HOME}/common/lib/activation.jar:${TOMCAT_HOME}/common/lib/pgjdbc2.jar:/home/cfs/html/WEB-INF/lib/darkhorseventures.jar:/home/cfs/html/WEB-INF/lib/theseus.jar
+export CLASSPATH=.:${TOMCAT_HOME}/common/lib/servlet.jar:${TOMCAT_HOME}/common/lib/mail.jar:${TOMCAT_HOME}/common/lib/activation.jar:${TOMCAT_HOME}/common/lib/pgjdbc2.jar:/home/cfs/html/WEB-INF/lib/darkhorseventures.jar:/home/cfs/html/WEB-INF/lib/theseus.jar:/home/cfs/html/WEB-INF/lib/zeroio-iteam.jar
