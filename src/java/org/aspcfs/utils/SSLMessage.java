c72ffe4bf3b32358a5a413136dfa0d24b878ca4c
mrajkowski <mrajkowski@concursive.com>
2002-03-15 20:17:01 +0000

    no message

--- src/java/org/aspcfs/utils/SSLMessage.java
+++ src/java/org/aspcfs/utils/SSLMessage.java
@@ -4,32 +4,68 @@ import java.net.*;
 import java.io.*;
 import javax.net.ssl.*;
 
+/**
+ *  Creates an SSLSocket connection to a remote SSL server, on the 
+ *  specified port.
+ *
+ *@author     Matt Rajkowski
+ *@created    March 15, 2002
+ */
 public class SSLMessage {
 
   private String url = null;
   private int port = -1;
   private String message = null;
 
+
+  /**
+   *  Constructor for the SSLMessage object
+   */
   public SSLMessage() { }
-  
+
+
+  /**
+   *  Sets the url attribute of the SSLMessage object
+   *
+   *@param  tmp  The new url value
+   */
   public void setUrl(String tmp) {
     url = tmp;
   }
-  
+
+
+  /**
+   *  Sets the port attribute of the SSLMessage object
+   *
+   *@param  tmp  The new port value
+   */
   public void setPort(int tmp) {
     port = tmp;
   }
-  
+
+
+  /**
+   *  Sets the message attribute of the SSLMessage object
+   *
+   *@param  tmp  The new message value
+   */
   public void setMessage(String tmp) {
     message = tmp;
   }
-  
+
+
+  /**
+   *  Description of the Method
+   *
+   *@return                Description of the Returned Value
+   *@exception  Exception  Description of Exception
+   */
   public int send() throws Exception {
 
     if (url == null || port < 0 || message == null) {
       return 1;
     }
-    
+
     try {
       //Prepare SSL... the host key needs to be in the keyring
       System.setProperty("java.protocol.handler.pkgs", "com.sun.net.ssl.internal.www.protocol");
@@ -45,16 +81,17 @@ public class SSLMessage {
       out.flush();
 
       //Receive response
-/*       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      String inputLine;
-      while ((inputLine = in.readLine()) != null && inputLine.indexOf("") == -1) {
-        System.out.println(inputLine);
-      }
-      if (inputLine != null) {
-        System.out.println(inputLine);
-      }
-      in.close();
- */
+      /*
+       *  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+       *  String inputLine;
+       *  while ((inputLine = in.readLine()) != null && inputLine.indexOf("") == -1) {
+       *  System.out.println(inputLine);
+       *  }
+       *  if (inputLine != null) {
+       *  System.out.println(inputLine);
+       *  }
+       *  in.close();
+       */
       out.close();
       socket.close();
       return 0;
@@ -64,3 +101,4 @@ public class SSLMessage {
     }
   }
 }
+
