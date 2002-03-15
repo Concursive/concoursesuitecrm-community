c6ff8d575cc2bccf29677d3c528e90a69050017d
mrajkowski <mrajkowski@concursive.com>
2002-03-15 15:04:09 +0000

    no message

--- src/web/javascript/editListForm.js
+++ src/web/javascript/editListForm.js
@@ -1,6 +1,23 @@
 <!--
 // need to set datatype for editing (searchField[index].type)
-
+function addValues(){
+    var searchList = document.modifyList.selectedList;
+    var count = 0;
+    var x = 0;
+    
+    searchText = document.modifyList.newValue.value;
+    
+    var newOption = searchText;
+    
+    if (searchList.length == 0 || searchList.options[0].value == "-1"){
+      searchList.options[0] = new Option(newOption);
+    } else {
+      searchList.options[searchList.length] = new Option(newOption, ("*" + searchText));
+    }
+    
+    document.modifyList.newValue.value = "";
+    document.modifyList.newValue.focus();
+}
 
 function removeValues(){
 	var searchList = document.modifyList.selectedList;
@@ -11,40 +28,36 @@ function removeValues(){
 	var searchCriteria = new Array();
 	
 	if (searchCriteria.length != searchList.length) {
-		for (count=0; count<(searchList.length); count++) {
-			searchCriteria[count] = searchList.options[count].value;
-		}
+          for (count=0; count<(searchList.length); count++) {
+                  searchCriteria[count] = searchList.options[count].value;
+          }
 	}
 	
 	if (searchList.length == 0) {
-		alert("Nothing to remove");
+          alert("Nothing to remove");
 	} else if (searchList.options.selectedIndex == -1) {
-    		alert("An item needs to be selected before it can be removed");
+          alert("An item needs to be selected before it can be removed");
   	} else {
-		searchCriteria[searchList.selectedIndex] = "skip";
-		searchList.options[searchList.selectedIndex] = null;
-		for (i=0; i < searchCriteria.length; i++){
-			if (searchCriteria[i] == "skip") {
-				offset = 1;
-				delete searchCriteria[i];
-				tempArray[i] = searchCriteria[i+offset];
-			}
-			else 
-				if (i+offset == searchCriteria.length) {
-					break;
-				}
-				else {
-					tempArray[i] = searchCriteria[i+offset];
-				}
-		}
-		delete searchCriteria
-		searchCriteria = new Array();
-		for (i=0; i < tempArray.length; i++){
-			if (tempArray[i] != null) {
-				searchCriteria[i] = tempArray[i];
-			}
-		}
-		
+          searchCriteria[searchList.selectedIndex] = "skip";
+          searchList.options[searchList.selectedIndex] = null;
+          for (i=0; i < searchCriteria.length; i++){
+            if (searchCriteria[i] == "skip") {
+              offset = 1;
+              delete searchCriteria[i];
+              tempArray[i] = searchCriteria[i+offset];
+            } else if (i+offset == searchCriteria.length) {
+              break;
+            } else {
+              tempArray[i] = searchCriteria[i+offset];
+            }
+          }
+          delete searchCriteria
+          searchCriteria = new Array();
+          for (i=0; i < tempArray.length; i++){
+            if (tempArray[i] != null) {
+              searchCriteria[i] = tempArray[i];
+            }
+          }
 	}
 }
 
@@ -53,36 +66,35 @@ function removeValues(){
 //  Swap positions of two options in a select list
 // -------------------------------------------------------------------
 function swapOptions(obj,i,j) {
-	var o = obj.options;
-	var i_selected = o[i].selected;
-	var j_selected = o[j].selected;
-	var temp = new Option(o[i].text, o[i].value, o[i].defaultSelected, o[i].selected);
-	var temp2= new Option(o[j].text, o[j].value, o[j].defaultSelected, o[j].selected);
-	o[i] = temp2;
-	o[j] = temp;
-	o[i].selected = j_selected;
-	o[j].selected = i_selected;
-	}
+  var o = obj.options;
+  var i_selected = o[i].selected;
+  var j_selected = o[j].selected;
+  var temp = new Option(o[i].text, o[i].value, o[i].defaultSelected, o[i].selected);
+  var temp2= new Option(o[j].text, o[j].value, o[j].defaultSelected, o[j].selected);
+  o[i] = temp2;
+  o[j] = temp;
+  o[i].selected = j_selected;
+  o[j].selected = i_selected;
+}
 	
 // -------------------------------------------------------------------
 //  Select All options
 // -------------------------------------------------------------------
 function selectAllOptions(obj) {
-	var size = obj.options.length;
-	var i = 0;
-	
-	if (size == 0) {
-		alert ("You must have at least one item in this list.");
-		return false;
-	}
-	
-	for (i=0;i<size;i++) {
-		obj.options[i].selected = true;
-		document.modifyList.selectNames.value = document.modifyList.selectNames.value + "^" + obj.options[i].text;
-	}
-	
-	//document.modifyList.submit();
-	return true;
+  var size = obj.options.length;
+  var i = 0;
+  
+  if (size == 0) {
+    alert ("You must have at least one item in this list.");
+    return false;
+  }
+  
+  for (i=0;i<size;i++) {
+    obj.options[i].selected = true;
+    document.modifyList.selectNames.value = document.modifyList.selectNames.value + "^" + obj.options[i].text;
+  }
+  
+  return true;
 }
 	
 // -------------------------------------------------------------------
@@ -90,72 +102,67 @@ function selectAllOptions(obj) {
 //  Move selected option in a select list up one
 // -------------------------------------------------------------------
 function moveOptionUp(obj) {
-	// If > 1 option selected, do nothing
-	var selectedCount=0;
-	for (i=0; i<obj.options.length; i++) {
-		if (obj.options[i].selected) {
-			selectedCount++;
-			}
-		}
-	if (selectedCount > 1) {
-		return;
-		}
-	// If this is the first item in the list, do nothing
-	var i = obj.selectedIndex;
-	if (i == 0) {
-		return;
-		}
-	swapOptions(obj,i,i-1);
-	obj.options[i-1].selected = true;
-	}
+  // If > 1 option selected, do nothing
+  var selectedCount=0;
+  for (i=0; i<obj.options.length; i++) {
+    if (obj.options[i].selected) {
+            selectedCount++;
+    }
+  }
+  if (selectedCount > 1) {
+    return;
+  }
+  // If this is the first item in the list, do nothing
+  var i = obj.selectedIndex;
+  if (i == 0) {
+    return;
+  }
+  swapOptions(obj,i,i-1);
+  obj.options[i-1].selected = true;
+}
 
 // -------------------------------------------------------------------
 // moveOptionDown(select_object)
 //  Move selected option in a select list down one
 // -------------------------------------------------------------------
 function moveOptionDown(obj) {
-	// If > 1 option selected, do nothing
-	var selectedCount=0;
-	for (i=0; i<obj.options.length; i++) {
-		if (obj.options[i].selected) {
-			selectedCount++;
-			}
-		}
-	if (selectedCount > 1) {
-		return;
-		}
-	// If this is the last item in the list, do nothing
-	var i = obj.selectedIndex;
-	if (i == (obj.options.length-1)) {
-		return;
-		}
-	swapOptions(obj,i,i+1);
-	obj.options[i+1].selected = true;
-	}
-	
+  // If > 1 option selected, do nothing
+  var selectedCount=0;
+  for (i=0; i<obj.options.length; i++) {
+    if (obj.options[i].selected) {
+      selectedCount++;
+    }
+  }
+  if (selectedCount > 1) {
+    return;
+  }
+  // If this is the last item in the list, do nothing
+  var i = obj.selectedIndex;
+  if (i == (obj.options.length-1)) {
+    return;
+  }
+  swapOptions(obj,i,i+1);
+  obj.options[i+1].selected = true;
+}
 	
 function sortSelect (select, compareFunction) {
-	if (!compareFunction)
-		compareFunction = compareText;
-	var options = new Array (select.options.length);
-	for (var i = 0; i < options.length; i++)
-		options[i] = 
-		new Option (
-		select.options[i].text,
-		select.options[i].value,
-		select.options[i].defaultSelected,
-		select.options[i].selected
-		);
-	
-	options.sort(compareFunction);
-	select.options.length = 0;
-	for (var i = 0; i < options.length; i++)
-		select.options[i] = options[i];
+  if (!compareFunction) compareFunction = compareText;
+  var options = new Array (select.options.length);
+  for (var i = 0; i < options.length; i++)
+    options[i] = new Option (
+          select.options[i].text,
+          select.options[i].value,
+          select.options[i].defaultSelected,
+          select.options[i].selected
+    );
+  
+  options.sort(compareFunction);
+  select.options.length = 0;
+  for (var i = 0; i < options.length; i++) select.options[i] = options[i];
 }
 
 function compareText (option1, option2) {
-	return option1.text < option2.text ? -1 :
-		option1.text > option2.text ? 1 : 0;
+  return option1.text < option2.text ? -1 : option1.text > option2.text ? 1 : 0;
 }
 	
 -->
