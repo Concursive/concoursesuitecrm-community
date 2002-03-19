d7d9602d158e202363d2e620423d25e2975ad205
mrajkowski <mrajkowski@concursive.com>
2002-03-19 21:03:31 +0000

    no message

--- src/sql/mssql/new_gatekeeper.sql
+++ src/sql/mssql/new_gatekeeper.sql
@@ -1,20 +1,23 @@
-/*
-	Gatekeeper database tables.
-	Assumes the user 'gatekeeper' is already created.
-*/
+/**
+ *  MSSQL Table Creation
+ *
+ *@author     mrajkowski
+ *@created    March 19, 2002
+ *@version    $Id$
+ */
 
 CREATE TABLE sites (
-  site_id SERIAL PRIMARY KEY,
+  site_id INT IDENTITY PRIMARY KEY,
   sitecode TEXT NOT NULL,				
   vhost TEXT NOT NULL DEFAULT '',	
   dbhost TEXT NOT NULL DEFAULT '',	
   dbname TEXT NOT NULL DEFAULT '',	
-  dbport INT NOT NULL DEFAULT 5432,	
+  dbport INT NOT NULL DEFAULT 1433,	
   dbuser TEXT NOT NULL DEFAULT '',
   dbpw TEXT NOT NULL DEFAULT '',
   driver TEXT NOT NULL DEFAULT '',
   code VARCHAR(255),
-  enabled BOOLEAN NOT NULL DEFAULT false
+  enabled BIT NOT NULL DEFAULT 0
 );
 
 GRANT ALL ON sites TO gatekeeper;
