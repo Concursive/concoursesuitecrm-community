0278f6a48c655e6436ac69e98c6f95c9b0383eaa
mrajkowski <mrajkowski@concursive.com>
2002-05-30 14:14:15 +0000

    no message

--- src/sql/postgresql/upgrade/2003-01-10.sql
+++ src/sql/postgresql/upgrade/2003-01-10.sql
@@ -1,16 +1,3 @@
-/* May 16, 2002  All servers up-to-date */
-
-CREATE TABLE access_log (
-  id serial,
-  user_id INT NOT NULL DEFAULT -1,
-  username VARCHAR(80) NOT NULL,
-  ip VARCHAR(15),
-  entered TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  browser VARCHAR(255)
-);
-
-UPDATE lookup_stage SET LEVEL = order_id;
-
-INSERT INTO permission (category_id, permission, permission_view, permission_add, permission_edit, permission_delete, description, level) VALUES (6, 'campaign-campaigns-surveys', true, true, true, true, 'Campaign Survey Records', 60);
+/* May 30, 2002  All PostgreSQL servers up-to-date */
 
 
