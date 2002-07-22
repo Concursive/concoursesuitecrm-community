879a62ec87499bd9b32dea7942c47b5d8571135b
chris <chris@concursive.com>
2002-07-22 19:02:31 +0000

    no message

--- src/java/org/aspcfs/modules/admin/base/User.java
+++ src/java/org/aspcfs/modules/admin/base/User.java
@@ -1681,14 +1681,13 @@ public void setRevenueLock(boolean revenueLock) {
     sql.append(
         "SELECT sum(rv.amount) as s " +
         "FROM revenue rv " +
-        "WHERE rv.owner in (?) AND rv.year = ? ");
+        "WHERE rv.owner IN (" + childUsers.getUserListIds(id) + ") AND rv.year = ? ");
 	
     if (type > 0) {
 	sql.append("AND rv.type = ? ");
     }
     pst = db.prepareStatement(sql.toString());
     int i = 0;
-    pst.setString(++i, childUsers.getUserListIds(id));
     pst.setInt(++i, year);
     if (type > 0) {
 	    pst.setInt(++i, type);
