4134bab218bd25602e23744c253edaf13b52bb8c
chris <chris@concursive.com>
2002-08-01 15:30:26 +0000

    no message

--- src/java/org/aspcfs/modules/healthcare/edit/actions/ProcessEditData.java
+++ src/java/org/aspcfs/modules/healthcare/edit/actions/ProcessEditData.java
@@ -48,7 +48,7 @@ public String executeCommandDefault(ActionContext context) {
       AuthenticationItem auth = new AuthenticationItem();
       db = auth.getConnection(context, false);
 		
-      StringTokenizer st = new com.Ostermiller.util.StringTokenizer(id, "|");
+      com.Ostermiller.util.StringTokenizer st = new com.Ostermiller.util.StringTokenizer(id, "|");
       String dbName = st.nextToken();
       String fileName = st.nextToken();
       
