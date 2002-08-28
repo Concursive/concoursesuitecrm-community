fc1a644574bf665958f544d471f4134599f6f3ca
chris <chris@concursive.com>
2002-08-28 16:06:12 +0000

    no message

--- src/sql/postgresql/new_revenue.sql
+++ src/sql/postgresql/new_revenue.sql
@@ -1,19 +1,3 @@
-CREATE TABLE revenue (
-  id serial PRIMARY KEY,
-  org_id int references organization(org_id),
-  transaction_id int default -1,
-  month int default -1,
-  year int default -1,
-  amount float default 0,
-  type int references lookup_revenue_types(code),
-  owner int references access(user_id),
-  description VARCHAR(255),
-  entered TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  enteredby INT NOT NULL references access(user_id),
-  modified TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  modifiedby INT NOT NULL references access(user_id)
-);
- 
 CREATE TABLE lookup_revenue_types (
   code SERIAL PRIMARY KEY,
   description VARCHAR(50) NOT NULL,
@@ -33,6 +17,22 @@ CREATE TABLE lookup_revenuedetail_types (
   enabled BOOLEAN DEFAULT true
 )
 ;
+CREATE TABLE revenue (
+  id serial PRIMARY KEY,
+  org_id int references organization(org_id),
+  transaction_id int default -1,
+  month int default -1,
+  year int default -1,
+  amount float default 0,
+  type int references lookup_revenue_types(code),
+  owner int references access(user_id),
+  description VARCHAR(255),
+  entered TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  enteredby INT NOT NULL references access(user_id),
+  modified TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modifiedby INT NOT NULL references access(user_id)
+);
+ 
 
 CREATE TABLE revenue_detail (
   id serial PRIMARY KEY,
