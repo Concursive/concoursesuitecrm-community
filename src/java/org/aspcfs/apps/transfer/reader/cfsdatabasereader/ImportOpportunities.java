aacdd15f0de95cc96b8fd73fbdc37ae3c4b5a5b2
chris <chris@concursive.com>
2002-09-06 14:13:29 +0000

    no message

---
+++ src/java/org/aspcfs/apps/transfer/reader/cfsdatabasereader/ImportOpportunities.java
@@ -0,0 +1,51 @@
+package com.darkhorseventures.apps.dataimport.cfsdatabasereader;
+
+import java.sql.*;
+import com.darkhorseventures.apps.dataimport.*;
+import com.darkhorseventures.cfsbase.*;
+import com.zeroio.iteam.base.*;
+import java.util.*;
+
+/**
+ *  Processes Opportunities
+ */
+public class ImportOpportunities implements CFSDatabaseReaderImportModule {
+  
+  DataWriter writer = null;
+  PropertyMapList mappings = null;
+  
+  /**
+   *  Description of the Method
+   *
+   *@param  writer  Description of the Parameter
+   *@param  db      Description of the Parameter
+   *@return         Description of the Return Value
+   */
+  public boolean process(DataWriter writer, Connection db, PropertyMapList mappings) throws SQLException {
+    this.writer = writer; 
+    this.mappings = mappings;
+    
+    //Loop through created items until complete, in the following order
+    OpportunityList oppList = new OpportunityList();
+    
+    writer.setAutoCommit(false);
+    this.saveOppList(db, oppList);
+    writer.commit();
+
+    //update owners
+    
+    writer.setAutoCommit(true);
+    return true;
+  }
+  
+  private void saveOppList(Connection db, OpportunityList oppList) {
+    Iterator opps = oppList.iterator();
+    while (opps.hasNext()) {
+      Opportunity thisOpp = (Opportunity)opps.next();
+      DataRecord thisRecord = mappings.createDataRecord(thisOpp, "insert");
+      writer.save(thisRecord);
+    }
+  }
+    
+}
+
