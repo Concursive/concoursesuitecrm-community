75ebd3cf00c27b2468258e96dd38b91cf7284da9
chris <chris@concursive.com>
2002-09-11 20:13:03 +0000

    no message

--- src/java/org/aspcfs/apps/transfer/reader/cfsdatabasereader/ImportBaseData.java
+++ src/java/org/aspcfs/apps/transfer/reader/cfsdatabasereader/ImportBaseData.java
@@ -188,7 +188,6 @@ public class ImportBaseData implements CFSDatabaseReaderImportModule {
       writer.save(thisRecord);
       writer.commit();
 
-      /**
       OrganizationEmailAddressList emailList = new OrganizationEmailAddressList();
       emailList.setOrgId(thisOrg.getId());
       emailList.buildList(db);
@@ -202,7 +201,6 @@ public class ImportBaseData implements CFSDatabaseReaderImportModule {
               writer.save(anotherRecord);
               writer.commit();
       }
-      */
       
       OrganizationAddressList addressList = new OrganizationAddressList();
       addressList.setOrgId(thisOrg.getId());
