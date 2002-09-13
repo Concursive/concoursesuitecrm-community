646612216fb425cdd3c43c8d8b416de407d08dd6
chris <chris@concursive.com>
2002-09-13 15:39:29 +0000

    no message

--- src/java/org/aspcfs/apps/transfer/reader/cfsdatabasereader/ImportCommunications.java
+++ src/java/org/aspcfs/apps/transfer/reader/cfsdatabasereader/ImportCommunications.java
@@ -47,6 +47,8 @@ public class ImportCommunications implements CFSDatabaseReaderImportModule {
     
     logger.info("ImportCommunications-> Inserting Saved Criteria Elements");
     processOK = ImportLookupTables.saveCustomLookupList(writer, db, mappings, "savedCriteriaElement");
+    System.out.println("result " + processOK);
+    
     if (!processOK) {
       return false;
     }
