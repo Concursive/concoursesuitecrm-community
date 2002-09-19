7e316ad1d1cb8fb837a000e04538f5acf638a7e8
chris <chris@concursive.com>
2002-09-19 19:55:38 +0000

    no message

--- src/java/org/aspcfs/modules/accounts/base/AccountTypeLevel.java
+++ src/java/org/aspcfs/modules/accounts/base/AccountTypeLevel.java
@@ -9,7 +9,7 @@ import com.darkhorseventures.utils.DatabaseUtils;
 
 public class AccountTypeLevel {
 
-  int id = -1;
+  int orgId = -1;
   int typeId = -1;
   int level = -1;
   java.sql.Timestamp entered = null;
@@ -25,8 +25,16 @@ public class AccountTypeLevel {
           queryRecord(db, id);
   }
   
-public void setId(int tmp) { this.id = tmp; }
-public void setId(String tmp) { this.id = Integer.parseInt(tmp); }
+public int getOrgId() {
+	return orgId;
+}
+public void setOrgId(int orgId) {
+	this.orgId = orgId;
+}
+
+public void setOrgId(String orgId) {
+	this.orgId = Intger.parseInt(orgId);
+}
 
 public void setTypeId(int tmp) { this.typeId = tmp; }
 public void setTypeId(String tmp) { this.typeId = Integer.parseInt(tmp); }
@@ -45,7 +53,6 @@ public void setModified(java.sql.Timestamp tmp) { this.modified = tmp; }
     this.modified = DateUtils.parseTimestampString(tmp);
   }
 
-public int getId() { return id; }
 public int getTypeId() { return typeId; }
 public int getLevel() { return level; }
 public java.sql.Timestamp getEntered() { return entered; }
@@ -57,10 +64,11 @@ public java.sql.Timestamp getModified() { return modified; }
                   throw new SQLException("Account Type Level entry not found.");
           }
           
-          PreparedStatement pst = db.prepareStatement("SELECT a.*, " +
+          PreparedStatement pst = db.prepareStatement("SELECT a.* " +
                 "FROM account_type_levels a " +
-                "WHERE a.id = ? ");
-          pst.setInt(1, id);
+                "WHERE a.org_id = ? and a.type_id = ? ");
+          pst.setInt(1, orgId);
+          pst.setInt(2, typeId);
           ResultSet rs = pst.executeQuery();
         if (rs.next()) {
                 buildRecord(rs);
@@ -78,7 +86,7 @@ public java.sql.Timestamp getModified() { return modified; }
     try {
       db.setAutoCommit(false);
       sql.append(
-          "INSERT INTO account_type_levels (id, type_id, ");
+          "INSERT INTO account_type_levels (org_id, type_id, ");
         if (entered != null) {
                 sql.append("entered, ");
         }
@@ -97,6 +105,11 @@ public java.sql.Timestamp getModified() { return modified; }
       
       int i = 0;
       PreparedStatement pst = db.prepareStatement(sql.toString());
+      if (orgId > -1) {
+	      pst.setInt(++i, this.getOrgId());
+      } else {
+	      pst.setNull(++i, java.sql.Types.INTEGER);
+      }
       if (typeId > -1) {
 	      pst.setInt(++i, this.getTypeId());
       } else {
@@ -113,7 +126,6 @@ public java.sql.Timestamp getModified() { return modified; }
       pst.execute();
       pst.close();
 
-      id = DatabaseUtils.getCurrVal(db, "account_type_levels_id_seq");
       db.commit();
     } catch (SQLException e) {
       db.rollback();
@@ -126,7 +138,10 @@ public java.sql.Timestamp getModified() { return modified; }
   }
 
   protected void buildRecord(ResultSet rs) throws SQLException {
-    id = rs.getInt("id");
+    orgId = rs.getInt("org_id");
+    if (rs.wasNull()) {
+            orgId = -1;
+    }    
     typeId = rs.getInt("type_id");
     if (rs.wasNull()) {
             typeId = -1;
