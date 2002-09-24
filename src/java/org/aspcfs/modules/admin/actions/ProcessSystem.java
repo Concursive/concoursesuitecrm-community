10144661116e36408aa172b98686142067b8dc7b
mrajkowski <mrajkowski@concursive.com>
2002-09-24 15:49:40 +0000

    no message

--- src/java/org/aspcfs/modules/admin/actions/ProcessSystem.java
+++ src/java/org/aspcfs/modules/admin/actions/ProcessSystem.java
@@ -15,14 +15,15 @@ import java.io.File;
  * @version $Id$
  */
 public final class ProcessSystem extends CFSModule {
-  /** Erases the day's graphing data since the graphs need to be rebuilt daily */  
+  
+  /** Erases the day's graphing data since the graphs need to be rebuilt daily */
   public String executeCommandClearGraphData(ActionContext context) {
     clearGraphData(context);
     deleteGraphFiles(context);
-    return null;
+    return "ProcessOK";
   }
   
-  /** Deletes all of the files in graphs should be run each night */  
+  /** Deletes all of the files in graphs, should be run each night */
   private void deleteGraphFiles(ActionContext context) {
     File graphFolder = new File(context.getServletContext().getRealPath("/") + "graphs" + fs);
     if (graphFolder.isDirectory()) {
@@ -35,25 +36,25 @@ public final class ProcessSystem extends CFSModule {
       }
     }
   }
-
-  /** Goes through each user and flags the graph data as invalid */  
+  
+  /** Goes through each user and flags the graph data as invalid */
   private void clearGraphData(ActionContext context) {
     Hashtable globalStatus = (Hashtable)context.getServletContext().getAttribute("SystemStatus");
-
+    
     Iterator i = globalStatus.values().iterator();
     UserList shortChildList = new UserList();
     UserList fullChildList = new UserList();
-
+    
     while (i.hasNext()) {
       SystemStatus thisStatus = (SystemStatus)i.next();
-
+      
       UserList thisList = thisStatus.getHierarchyList();
       Iterator j = thisList.iterator();
       while (j.hasNext()) {
         User thisUser = (User)j.next();
         shortChildList = thisUser.getShortChildList();
         fullChildList = thisUser.getFullChildList(shortChildList, new UserList());
-
+        
         Iterator k = fullChildList.iterator();
         while (k.hasNext()) {
           User indUser = (User)k.next();
