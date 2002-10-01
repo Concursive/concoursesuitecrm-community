9748196dd3abd97160401cbbcf88549d84d21097
mrajkowski <mrajkowski@concursive.com>
2002-10-01 19:58:00 +0000

    Changed default sort order

--- src/java/org/aspcfs/modules/accounts/base/RevenueList.java
+++ src/java/org/aspcfs/modules/accounts/base/RevenueList.java
@@ -12,7 +12,7 @@ import com.darkhorseventures.webutils.HtmlSelect;
 import com.darkhorseventures.utils.DatabaseUtils;
 
 public class RevenueList extends Vector {
-
+  
   private int orgId = -1;
   private int type = 0;
   private String ownerIdRange = null;
@@ -25,31 +25,31 @@ public class RevenueList extends Vector {
   private java.sql.Timestamp nextAnchor = null;
   private int syncType = Constants.NO_SYNC;
   private PagedListInfo pagedListInfo = null;
-
+  
   public RevenueList() { }
-
+  
   public void buildList(Connection db) throws SQLException {
-
+    
     PreparedStatement pst = null;
     ResultSet rs = null;
     int items = -1;
-
+    
     StringBuffer sqlSelect = new StringBuffer();
     StringBuffer sqlCount = new StringBuffer();
     StringBuffer sqlFilter = new StringBuffer();
     StringBuffer sqlOrder = new StringBuffer();
-
+    
     //Need to build a base SQL statement for counting records
     sqlCount.append(
-      "SELECT COUNT(*) AS recordcount " +
-        "FROM revenue r " +
-        "LEFT JOIN contact ct_eb ON (r.enteredby = ct_eb.user_id) " +
-        "LEFT JOIN contact ct_mb ON (r.modifiedby = ct_mb.user_id) " +
-	"LEFT JOIN contact ct_own ON (r.owner = ct_own.user_id) " +
-	"LEFT JOIN lookup_revenue_types rt ON (r.type = rt.code) " +
-	"LEFT JOIN organization o ON (r.org_id = o.org_id) " +
-        "WHERE r.id > -1 ");
-
+    "SELECT COUNT(*) AS recordcount " +
+    "FROM revenue r " +
+    "LEFT JOIN contact ct_eb ON (r.enteredby = ct_eb.user_id) " +
+    "LEFT JOIN contact ct_mb ON (r.modifiedby = ct_mb.user_id) " +
+    "LEFT JOIN contact ct_own ON (r.owner = ct_own.user_id) " +
+    "LEFT JOIN lookup_revenue_types rt ON (r.type = rt.code) " +
+    "LEFT JOIN organization o ON (r.org_id = o.org_id) " +
+    "WHERE r.id > -1 ");
+    
     createFilter(sqlFilter);
     
     if (pagedListInfo != null) {
@@ -61,17 +61,14 @@ public class RevenueList extends Vector {
         int maxRecords = rs.getInt("recordcount");
         pagedListInfo.setMaxRecords(maxRecords);
       }
-      
-      
-      
       pst.close();
       rs.close();
-
+      
       //Determine the offset, based on the filter, for the first record to show
       if (!pagedListInfo.getCurrentLetter().equals("")) {
         pst = db.prepareStatement(sqlCount.toString() +
-            sqlFilter.toString() +
-            "AND r.description < ? ");
+        sqlFilter.toString() +
+        "AND r.description < ? ");
         items = prepareFilter(pst);
         pst.setString(++items, pagedListInfo.getCurrentLetter().toLowerCase());
         rs = pst.executeQuery();
@@ -84,15 +81,15 @@ public class RevenueList extends Vector {
       }
       
       //Determine column to sort by
-      pagedListInfo.setDefaultSort("r.description", null);
+      pagedListInfo.setDefaultSort("r.year,r.month", "desc");
       pagedListInfo.appendSqlTail(db, sqlOrder);
     } else {
-      sqlOrder.append("ORDER BY r.description ");
+      sqlOrder.append("ORDER BY r.year desc,r.month desc ");
     }
     
     
     //System.out.println(sqlOrder.toString());
-
+    
     //Need to build a base SQL statement for returning records
     if (pagedListInfo != null) {
       pagedListInfo.appendSqlSelectHead(db, sqlSelect);
@@ -100,31 +97,31 @@ public class RevenueList extends Vector {
       sqlSelect.append("SELECT ");
     }
     sqlSelect.append(
-        "r.*, " +
-        "ct_eb.namelast as eb_namelast, ct_eb.namefirst as eb_namefirst, " +
-        "ct_mb.namelast as mb_namelast, ct_mb.namefirst as mb_namefirst, ct_own.namelast as own_namelast, ct_own.namefirst as own_namefirst, rt.description as typename, o.name as orgname " +
-        "FROM revenue r " +
-        "LEFT JOIN contact ct_eb ON (r.enteredby = ct_eb.user_id) " +
-        "LEFT JOIN contact ct_mb ON (r.modifiedby = ct_mb.user_id) " +
-	"LEFT JOIN contact ct_own ON (r.owner = ct_own.user_id) " +
-	"LEFT JOIN organization o ON (r.org_id = o.org_id) " +
-	"LEFT JOIN lookup_revenue_types rt ON (r.type = rt.code) " +
-        "WHERE r.id > -1 ");
-
+      "r.*, " +
+      "ct_eb.namelast as eb_namelast, ct_eb.namefirst as eb_namefirst, " +
+      "ct_mb.namelast as mb_namelast, ct_mb.namefirst as mb_namefirst, ct_own.namelast as own_namelast, ct_own.namefirst as own_namefirst, rt.description as typename, o.name as orgname " +
+      "FROM revenue r " +
+      "LEFT JOIN contact ct_eb ON (r.enteredby = ct_eb.user_id) " +
+      "LEFT JOIN contact ct_mb ON (r.modifiedby = ct_mb.user_id) " +
+      "LEFT JOIN contact ct_own ON (r.owner = ct_own.user_id) " +
+      "LEFT JOIN organization o ON (r.org_id = o.org_id) " +
+      "LEFT JOIN lookup_revenue_types rt ON (r.type = rt.code) " +
+      "WHERE r.id > -1 ");
+    
     pst = db.prepareStatement(sqlSelect.toString() + sqlFilter.toString() + sqlOrder.toString());
     items = prepareFilter(pst);
     //System.out.println(pst.toString());
     rs = pst.executeQuery();
-
+    
     if (pagedListInfo != null) {
       pagedListInfo.doManualOffset(db, rs);
     }
-
+    
     int count = 0;
     while (rs.next()) {
       if (pagedListInfo != null && pagedListInfo.getItemsPerPage() > 0 &&
-          DatabaseUtils.getType(db) == DatabaseUtils.MSSQL &&
-          count >= pagedListInfo.getItemsPerPage()) {
+      DatabaseUtils.getType(db) == DatabaseUtils.MSSQL &&
+      count >= pagedListInfo.getItemsPerPage()) {
         break;
       }
       ++count;
@@ -142,54 +139,54 @@ public class RevenueList extends Vector {
       thisRevenue.delete(db);
     }
   }
-    public String getTableName() { return tableName; }
-public String getUniqueField() { return uniqueField; }
-public java.sql.Timestamp getLastAnchor() { return lastAnchor; }
-public java.sql.Timestamp getNextAnchor() { return nextAnchor; }
-public int getSyncType() { return syncType; }
-public void setLastAnchor(java.sql.Timestamp tmp) { this.lastAnchor = tmp; }
-public void setNextAnchor(java.sql.Timestamp tmp) { this.nextAnchor = tmp; }
-public void setSyncType(int tmp) { this.syncType = tmp; }
-
+  public String getTableName() { return tableName; }
+  public String getUniqueField() { return uniqueField; }
+  public java.sql.Timestamp getLastAnchor() { return lastAnchor; }
+  public java.sql.Timestamp getNextAnchor() { return nextAnchor; }
+  public int getSyncType() { return syncType; }
+  public void setLastAnchor(java.sql.Timestamp tmp) { this.lastAnchor = tmp; }
+  public void setNextAnchor(java.sql.Timestamp tmp) { this.nextAnchor = tmp; }
+  public void setSyncType(int tmp) { this.syncType = tmp; }
+  
   public int getOrgId() {
-	return orgId;
-}
-public void setOrgId(int orgId) {
-	this.orgId = orgId;
-}
-public int getType() {
-	return type;
-}
-public void setType(int type) {
-	this.type = type;
-}
-public int getYear() {
-	return year;
-}
-public void setYear(int year) {
-	this.year = year;
-}
-
-public void setPagedListInfo(PagedListInfo pagedListInfo) {
-	this.pagedListInfo = pagedListInfo;
-}
-public PagedListInfo getPagedListInfo() {
-	return pagedListInfo;
-}
-
-public String getOwnerIdRange() {
-	return ownerIdRange;
-}
-public void setOwnerIdRange(String ownerIdRange) {
-	this.ownerIdRange = ownerIdRange;
-}
-public int getOwner() {
-	return owner;
-}
-public void setOwner(int owner) {
-	this.owner = owner;
-}
-
+    return orgId;
+  }
+  public void setOrgId(int orgId) {
+    this.orgId = orgId;
+  }
+  public int getType() {
+    return type;
+  }
+  public void setType(int type) {
+    this.type = type;
+  }
+  public int getYear() {
+    return year;
+  }
+  public void setYear(int year) {
+    this.year = year;
+  }
+  
+  public void setPagedListInfo(PagedListInfo pagedListInfo) {
+    this.pagedListInfo = pagedListInfo;
+  }
+  public PagedListInfo getPagedListInfo() {
+    return pagedListInfo;
+  }
+  
+  public String getOwnerIdRange() {
+    return ownerIdRange;
+  }
+  public void setOwnerIdRange(String ownerIdRange) {
+    this.ownerIdRange = ownerIdRange;
+  }
+  public int getOwner() {
+    return owner;
+  }
+  public void setOwner(int owner) {
+    this.owner = owner;
+  }
+  
   private void createFilter(StringBuffer sqlFilter) {
     if (sqlFilter == null) {
       sqlFilter = new StringBuffer();
@@ -210,10 +207,10 @@ public void setOwner(int owner) {
       sqlFilter.append("AND r.owner in (" + this.ownerIdRange + ") ");
     }
   }
-
+  
   private int prepareFilter(PreparedStatement pst) throws SQLException {
     int i = 0;
-      
+    
     if (orgId != -1) {
       pst.setInt(++i, orgId);
     }
@@ -227,7 +224,7 @@ public void setOwner(int owner) {
     if (year > -1) {
       pst.setInt(++i, year);
     }
-      
+    
     return i;
   }
 }
