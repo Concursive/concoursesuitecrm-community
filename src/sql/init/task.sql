e97a0205d8d4139e4b04a9e27f49c03f5d9d7a65
mrajkowski <mrajkowski@concursive.com>
2002-10-07 20:54:54 +0000

    no message

--- src/sql/init/task.sql
+++ src/sql/init/task.sql
@@ -1,8 +1,8 @@
-INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Minute(s)', false, 60);
-INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Hour(s)', true, 3600);
-INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Day(s)', false, 86400);
-INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Week(s)', false, 604800);
-INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Month(s)', false, 18144000);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (1, 'Minute(s)', false);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (1, 'Hour(s)', true);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (1, 'Day(s)', false);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (1, 'Week(s)', false);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (1, 'Month(s)', false);
 
 INSERT INTO lookup_task_priority (level, description, default_item) VALUES (1, '1', true);
 INSERT INTO lookup_task_priority (level, description) VALUES (2, '2');
