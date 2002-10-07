83e5390c7e18167fda74eccd6c0414af10b1a8f0
mrajkowski <mrajkowski@concursive.com>
2002-10-07 20:49:34 +0000

    Default settings for Task lookup tables

---
+++ src/sql/init/task.sql
@@ -0,0 +1,12 @@
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Minute(s)', false, 60);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Hour(s)', true, 3600);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Day(s)', false, 86400);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Week(s)', false, 604800);
+INSERT INTO lookup_task_loe (level, description, default_item) VALUES (0, 1, 'Month(s)', false, 18144000);
+
+INSERT INTO lookup_task_priority (level, description, default_item) VALUES (1, '1', true);
+INSERT INTO lookup_task_priority (level, description) VALUES (2, '2');
+INSERT INTO lookup_task_priority (level, description) VALUES (3, '3');
+INSERT INTO lookup_task_priority (level, description) VALUES (4, '4');
+INSERT INTO lookup_task_priority (level, description) VALUES (5, '5');
+
