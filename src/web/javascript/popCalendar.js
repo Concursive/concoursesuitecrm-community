75ed07a2e7af8bfdd425a167cde130deb8c3f1b1
akhi_m <akhi_m@concursive.com>
2002-10-08 21:02:15 +0000

    changed name of window from calendar to popcalendar

--- src/web/javascript/popCalendar.js
+++ src/web/javascript/popCalendar.js
@@ -22,7 +22,7 @@ function popCalendar(formname, element) {
   var posx = (screen.width - 190)/2;
   var posy = (screen.height - 200)/2;
   
-  var newwin=window.open(filename, 'calendar', 'WIDTH=190,HEIGHT=200,RESIZABLE=yes,SCROLLBARS=no,STATUS=0,LEFT=' + posx + ',TOP=' + posy + ',screenx=' + posx + ',screeny=' + posy);
+  var newwin=window.open(filename, 'popcalendar', 'WIDTH=190,HEIGHT=200,RESIZABLE=yes,SCROLLBARS=no,STATUS=0,LEFT=' + posx + ',TOP=' + posy + ',screenx=' + posx + ',screeny=' + posy);
   if (newwin != null) {
     if (newwin.opener == null)
       newwin.opener = self;
