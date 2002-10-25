62f04e16cb991b414531e5c859694a02d0838ff9
mrajkowski <mrajkowski@concursive.com>
2002-10-25 18:39:15 +0000

    Initial objects to store sync info -- not tested

---
+++ src/java/org/aspcfs/modules/service/base/SyncTransactionLog.java
@@ -0,0 +1,358 @@
+//Copyright 2002 Dark Horse Ventures
+
+package com.darkhorseventures.cfsbase;
+
+import org.theseus.beans.*;
+import java.util.*;
+import java.sql.*;
+import java.text.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.darkhorseventures.utils.DatabaseUtils;
+import com.darkhorseventures.utils.DateUtils;
+
+/**
+ *  Everytime a client uses the sync API, the action is logged. Each action
+ *  is made up of one or more transaction requests that result in a success or
+ *  failure.
+ *
+ *@author     matt rajkowski
+ *@created    October 25, 2002
+ *@version    $Id$
+ */
+public class SyncTransactionLog extends ArrayList {
+
+  private int id = -1;
+  private int systemId = -1;
+  private int clientId = -1;
+  private String ip = null;
+  private java.sql.Timestamp entered = null;
+
+
+  /**
+   *  Constructor for the SyncTransactionLog object
+   */
+  public SyncTransactionLog() { }
+
+
+  /**
+   *  Constructor for the SyncTransactionLog object
+   *
+   *@param  rs                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public SyncTransactionLog(ResultSet rs) throws SQLException {
+    buildRecord(rs);
+  }
+
+
+  /**
+   *  Constructor for the SyncTransactionLog object
+   *
+   *@param  db                Description of the Parameter
+   *@param  id                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public SyncTransactionLog(Connection db, int id) throws SQLException {
+    queryRecord(db, id);
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@param  id                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void queryRecord(Connection db, int id) throws SQLException {
+    if (id == -1) {
+      throw new SQLException("Invalid Transaction Log ID");
+    }
+
+    PreparedStatement pst = db.prepareStatement(
+        "SELECT sl.* " +
+        "FROM sync_log sl " +
+        "WHERE sl.log_id = ? ");
+    pst.setInt(1, id);
+    ResultSet rs = pst.executeQuery();
+    if (rs.next()) {
+      buildRecord(rs);
+    } else {
+      rs.close();
+      pst.close();
+      throw new SQLException("Sync Transaction Log not found");
+    }
+    rs.close();
+    pst.close();
+  }
+
+
+  /**
+   *  Sets the id attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new id value
+   */
+  public void setId(int tmp) {
+    this.id = tmp;
+  }
+
+
+  /**
+   *  Sets the id attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new id value
+   */
+  public void setId(String tmp) {
+    this.id = Integer.parseInt(tmp);
+  }
+
+
+  /**
+   *  Sets the systemId attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new systemId value
+   */
+  public void setSystemId(int tmp) {
+    this.systemId = tmp;
+  }
+
+
+  /**
+   *  Sets the systemId attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new systemId value
+   */
+  public void setSystemId(String tmp) {
+    this.systemId = Integer.parseInt(tmp);
+  }
+
+
+  /**
+   *  Sets the clientId attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new clientId value
+   */
+  public void setClientId(int tmp) {
+    this.clientId = tmp;
+  }
+
+
+  /**
+   *  Sets the clientId attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new clientId value
+   */
+  public void setClientId(String tmp) {
+    this.clientId = Integer.parseInt(tmp);
+  }
+
+
+  /**
+   *  Sets the ip attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new ip value
+   */
+  public void setIp(String tmp) {
+    this.ip = tmp;
+  }
+
+
+  /**
+   *  Sets the entered attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new entered value
+   */
+  public void setEntered(java.sql.Timestamp tmp) {
+    this.entered = tmp;
+  }
+
+
+  /**
+   *  Sets the entered attribute of the SyncTransactionLog object
+   *
+   *@param  tmp  The new entered value
+   */
+  public void setEntered(String tmp) {
+    this.entered = DateUtils.parseTimestampString(tmp);
+  }
+
+
+  /**
+   *  Gets the id attribute of the SyncTransactionLog object
+   *
+   *@return    The id value
+   */
+  public int getId() {
+    return id;
+  }
+
+
+  /**
+   *  Gets the systemId attribute of the SyncTransactionLog object
+   *
+   *@return    The systemId value
+   */
+  public int getSystemId() {
+    return systemId;
+  }
+
+
+  /**
+   *  Gets the clientId attribute of the SyncTransactionLog object
+   *
+   *@return    The clientId value
+   */
+  public int getClientId() {
+    return clientId;
+  }
+
+
+  /**
+   *  Gets the ip attribute of the SyncTransactionLog object
+   *
+   *@return    The ip value
+   */
+  public String getIp() {
+    return ip;
+  }
+
+
+  /**
+   *  Gets the entered attribute of the SyncTransactionLog object
+   *
+   *@return    The entered value
+   */
+  public java.sql.Timestamp getEntered() {
+    return entered;
+  }
+
+
+  /**
+   *  Gets the enteredString attribute of the SyncTransactionLog object
+   *
+   *@return    The enteredString value
+   */
+  public String getEnteredString() {
+    String tmp = "";
+    try {
+      return DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG).format(entered);
+    } catch (NullPointerException e) {
+    }
+    return tmp;
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void insert(Connection db) throws SQLException {
+    if (systemId == -1 || clientId == -1) {
+      throw new SQLException("Log Entry must be associated with a System and Client");
+    }
+
+    StringBuffer sql = new StringBuffer();
+    try {
+      db.setAutoCommit(false);
+      sql.append(
+          "INSERT INTO sync_log (system_id, client_id, ip ");
+      if (entered != null) {
+        sql.append(", entered ");
+      }
+      sql.append(") ");
+      sql.append("VALUES (?, ?, ? ");
+      if (entered != null) {
+        sql.append(", ? ");
+      }
+      sql.append(") ");
+
+      int i = 0;
+      PreparedStatement pst = db.prepareStatement(sql.toString());
+      pst.setInt(++i, systemId);
+      pst.setInt(++i, clientId);
+      pst.setString(++i, ip);
+      if (entered != null) {
+        pst.setTimestamp(++i, entered);
+      }
+      pst.execute();
+      pst.close();
+
+      id = DatabaseUtils.getCurrVal(db, "sync_log_log_id_seq");
+
+      if (this.size() > 0) {
+        Iterator items = this.iterator();
+        while (items.hasNext()) {
+          SyncTransactionLogItem thisItem = (SyncTransactionLogItem) items.next();
+          thisItem.insert(db);
+        }
+      }
+
+      db.commit();
+    } catch (SQLException e) {
+      db.rollback();
+      db.setAutoCommit(true);
+      throw new SQLException(e.getMessage());
+    } finally {
+      db.setAutoCommit(true);
+    }
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@return                   Description of the Return Value
+   *@exception  SQLException  Description of the Exception
+   */
+  public boolean delete(Connection db) throws SQLException {
+    if (this.getId() == -1) {
+      throw new SQLException("Access Log ID not specified.");
+    }
+
+    try {
+      db.setAutoCommit(false);
+
+      if (this.size() > 0) {
+        Iterator items = this.iterator();
+        while (items.hasNext()) {
+          SyncTransactionLogItem thisItem = (SyncTransactionLogItem) items.next();
+          thisItem.delete(db);
+        }
+      }
+
+      PreparedStatement pst = db.prepareStatement(
+          "DELETE FROM sync_log " +
+          "WHERE log_id = ? ");
+      pst.setInt(1, id);
+      pst.executeUpdate();
+      pst.close();
+      db.commit();
+    } catch (SQLException e) {
+      db.rollback();
+    } finally {
+      db.setAutoCommit(true);
+    }
+    return true;
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  rs                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  protected void buildRecord(ResultSet rs) throws SQLException {
+    id = rs.getInt("log_id");
+    systemId = rs.getInt("system_id");
+    clientId = rs.getInt("client_id");
+    ip = rs.getString("ip");
+    entered = rs.getTimestamp("entered");
+  }
+
+}
+
---
+++ src/java/org/aspcfs/modules/service/base/SyncTransactionLogItem.java
@@ -0,0 +1,392 @@
+//Copyright 2002 Dark Horse Ventures
+
+package com.darkhorseventures.cfsbase;
+
+import java.sql.*;
+import com.darkhorseventures.utils.DatabaseUtils;
+
+/**
+ *  For each sync API request, this class represents one or more of the
+ *  transactions that occurs.  An action can result in records so that count
+ *  can be stored.
+ *
+ *@author     matt rajkowski
+ *@created    October 25, 2002
+ *@version    $Id$
+ */
+public class SyncTransactionLogItem {
+
+  private int id = -1;
+  private int logId = -1;
+  private String referenceId = null;
+  private String elementName = null;
+  private String action = null;
+  private int linkItemId = -1;
+  private int statusCode = -1;
+  private int recordCount = -1;
+  private String message = null;
+
+
+  /**
+   *  Constructor for the SyncTransactionLogItem object
+   */
+  public SyncTransactionLogItem() { }
+
+
+  /**
+   *  Constructor for the SyncTransactionLogItem object
+   *
+   *@param  rs                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public SyncTransactionLogItem(ResultSet rs) throws SQLException {
+    buildRecord(rs);
+  }
+
+
+  /**
+   *  Constructor for the SyncTransactionLogItem object
+   *
+   *@param  db                Description of the Parameter
+   *@param  id                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public SyncTransactionLogItem(Connection db, int id) throws SQLException {
+    queryRecord(db, id);
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@param  id                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void queryRecord(Connection db, int id) throws SQLException {
+    if (id == -1) {
+      throw new SQLException("Invalid Transaction Log Item ID");
+    }
+
+    PreparedStatement pst = db.prepareStatement(
+        "SELECT stl.* " +
+        "FROM sync_transaction_log stl " +
+        "WHERE stl.transaction_id = ? ");
+    pst.setInt(1, id);
+    ResultSet rs = pst.executeQuery();
+    if (rs.next()) {
+      buildRecord(rs);
+    } else {
+      rs.close();
+      pst.close();
+      throw new SQLException("Sync Transaction Log not found");
+    }
+    rs.close();
+    pst.close();
+  }
+
+
+  /**
+   *  Sets the id attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new id value
+   */
+  public void setId(int tmp) {
+    this.id = tmp;
+  }
+
+
+  /**
+   *  Sets the id attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new id value
+   */
+  public void setId(String tmp) {
+    this.id = Integer.parseInt(tmp);
+  }
+
+
+  /**
+   *  Sets the logId attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new logId value
+   */
+  public void setLogId(int tmp) {
+    this.logId = tmp;
+  }
+
+
+  /**
+   *  Sets the logId attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new logId value
+   */
+  public void setLogId(String tmp) {
+    this.logId = Integer.parseInt(tmp);
+  }
+
+
+  /**
+   *  Sets the referenceId attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new referenceId value
+   */
+  public void setReferenceId(String tmp) {
+    this.referenceId = tmp;
+  }
+
+
+  /**
+   *  Sets the elementName attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new elementName value
+   */
+  public void setElementName(String tmp) {
+    this.elementName = tmp;
+  }
+
+
+  /**
+   *  Sets the action attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new action value
+   */
+  public void setAction(String tmp) {
+    this.action = tmp;
+  }
+
+
+  /**
+   *  Sets the linkItemId attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new linkItemId value
+   */
+  public void setLinkItemId(int tmp) {
+    this.linkItemId = tmp;
+  }
+
+
+  /**
+   *  Sets the linkItemId attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new linkItemId value
+   */
+  public void setLinkItemId(String tmp) {
+    this.linkItemId = Integer.parseInt(tmp);
+  }
+
+
+  /**
+   *  Sets the statusCode attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new statusCode value
+   */
+  public void setStatusCode(int tmp) {
+    this.statusCode = tmp;
+  }
+
+
+  /**
+   *  Sets the statusCode attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new statusCode value
+   */
+  public void setStatusCode(String tmp) {
+    this.statusCode = Integer.parseInt(tmp);
+  }
+
+
+  /**
+   *  Sets the recordCount attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new recordCount value
+   */
+  public void setRecordCount(int tmp) {
+    this.recordCount = tmp;
+  }
+
+
+  /**
+   *  Sets the recordCount attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new recordCount value
+   */
+  public void setRecordCount(String tmp) {
+    this.recordCount = Integer.parseInt(tmp);
+  }
+
+
+  /**
+   *  Sets the message attribute of the SyncTransactionLogItem object
+   *
+   *@param  tmp  The new message value
+   */
+  public void setMessage(String tmp) {
+    this.message = tmp;
+  }
+
+
+  /**
+   *  Gets the id attribute of the SyncTransactionLogItem object
+   *
+   *@return    The id value
+   */
+  public int getId() {
+    return id;
+  }
+
+
+  /**
+   *  Gets the logId attribute of the SyncTransactionLogItem object
+   *
+   *@return    The logId value
+   */
+  public int getLogId() {
+    return logId;
+  }
+
+
+  /**
+   *  Gets the referenceId attribute of the SyncTransactionLogItem object
+   *
+   *@return    The referenceId value
+   */
+  public String getReferenceId() {
+    return referenceId;
+  }
+
+
+  /**
+   *  Gets the elementName attribute of the SyncTransactionLogItem object
+   *
+   *@return    The elementName value
+   */
+  public String getElementName() {
+    return elementName;
+  }
+
+
+  /**
+   *  Gets the action attribute of the SyncTransactionLogItem object
+   *
+   *@return    The action value
+   */
+  public String getAction() {
+    return action;
+  }
+
+
+  /**
+   *  Gets the linkItemId attribute of the SyncTransactionLogItem object
+   *
+   *@return    The linkItemId value
+   */
+  public int getLinkItemId() {
+    return linkItemId;
+  }
+
+
+  /**
+   *  Gets the statusCode attribute of the SyncTransactionLogItem object
+   *
+   *@return    The statusCode value
+   */
+  public int getStatusCode() {
+    return statusCode;
+  }
+
+
+  /**
+   *  Gets the recordCount attribute of the SyncTransactionLogItem object
+   *
+   *@return    The recordCount value
+   */
+  public int getRecordCount() {
+    return recordCount;
+  }
+
+
+  /**
+   *  Gets the message attribute of the SyncTransactionLogItem object
+   *
+   *@return    The message value
+   */
+  public String getMessage() {
+    return message;
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void insert(Connection db) throws SQLException {
+    if (logId == -1) {
+      throw new SQLException("Log Transaction entry must be associated with a Log");
+    }
+
+    String sql =
+        "INSERT INTO sync_transaction_log " +
+        "(log_id, reference_id, element_name, " +
+        "action, link_item_id, status_code, record_count, message) VALUES " +
+        "(?, ?, ?, ?, ?, ?, ?, ?)";
+    int i = 0;
+    PreparedStatement pst = db.prepareStatement(sql);
+    pst.setInt(++i, logId);
+    pst.setString(++i, referenceId);
+    pst.setString(++i, elementName);
+    pst.setString(++i, action);
+    DatabaseUtils.setInt(pst, ++i, linkItemId);
+    DatabaseUtils.setInt(pst, ++i, statusCode);
+    DatabaseUtils.setInt(pst, ++i, recordCount);
+    pst.setString(++i, message);
+    pst.execute();
+    pst.close();
+
+    id = DatabaseUtils.getCurrVal(db, "sync_transact_transaction_i_seq");
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@return                   Description of the Return Value
+   *@exception  SQLException  Description of the Exception
+   */
+  public boolean delete(Connection db) throws SQLException {
+    if (this.getId() == -1) {
+      throw new SQLException("Transaction Log ID not specified.");
+    }
+    PreparedStatement pst = db.prepareStatement(
+        "DELETE FROM sync_transaction_log " +
+        "WHERE transaction_id = ? ");
+    pst.setInt(1, id);
+    pst.executeUpdate();
+    pst.close();
+    return true;
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  rs                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  protected void buildRecord(ResultSet rs) throws SQLException {
+    id = rs.getInt("transaction_id");
+    logId = rs.getInt("log_id");
+    referenceId = rs.getString("reference_id");
+    elementName = rs.getString("element_name");
+    action = rs.getString("action");
+    linkItemId = DatabaseUtils.getInt(rs, "link_item_id");
+    statusCode = DatabaseUtils.getInt(rs, "status_code");
+    recordCount = DatabaseUtils.getInt(rs, "record_count");
+    message = rs.getString("message");
+  }
+
+}
+
