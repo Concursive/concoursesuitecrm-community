0f8fb77583d0195d5b3dc344f83ed86ca7624eca
mrajkowski <mrajkowski@concursive.com>
2002-12-11 20:16:50 +0000

    Updated to match production server

--- env.cb2.sh
+++ env.cb2.sh
@@ -9,7 +9,9 @@ export JAVA_HOME=/usr/local/java/j2se
 export ANT_HOME=/usr/local/java/ant
 PATH=$PATH:${JAVA_HOME}/bin:${ANT_HOME}/bin
 
-export CLASSPATH=.:${TOMCAT_HOME}/common/lib/servlet.jar:${TOMCAT_HOME}/common/lib/mail.jar:${TOMCAT_HOME}/common/lib/activation.jar:${TOMCAT_HOME}/common/lib/pgjdbc2.jar:/home/cfs/html/WEB-INF/lib/darkhorseventures.jar:/home/cfs/html/WEB-INF/lib/theseus.jar:/home/cfs/html/WEB-INF/lib/zeroio-iteam.jar
+export SITE_LIB=/home/cfs/html/WEB-INF/lib
+
+export CLASSPATH=.:${TOMCAT_HOME}/common/lib/servlet.jar:${TOMCAT_HOME}/common/lib/mail.jar:${TOMCAT_HOME}/common/lib/mailapi.jar:${TOMCAT_HOME}/common/lib/smtp.jar:${TOMCAT_HOME}/lib/xerces.jar:${SITE_LIB}/darkhorseventures.jar:${SITE_LIB}/theseus.jar:${TOMCAT_HOME}/common/lib/activation.jar:${TOMCAT_HOME}/common/lib/pgjdbc2.jar:${SITE_LIB}/zeroio-iteam.jar
 
 # Site specific variables during installation
 GKDRIVER="org.postgresql.Driver"
