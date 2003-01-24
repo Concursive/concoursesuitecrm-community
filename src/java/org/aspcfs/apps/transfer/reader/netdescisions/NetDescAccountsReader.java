bdbe3b96b1baebd80fc683f3f956a7ac5436f2d5
akhi_m <akhi_m@concursive.com>
2003-01-24 22:51:06 +0000

    netdescisions accounts  transfer

--- src/java/org/aspcfs/apps/transfer/reader/netdescisions/NetDescAccountsReader.java
+++ src/java/org/aspcfs/apps/transfer/reader/netdescisions/NetDescAccountsReader.java
@@ -130,11 +130,14 @@ public class NetDescAccountsReader implements DataReader {
    *@return    The configured value
    */
   public boolean isConfigured() {
+    logger.info("Checking Reader Configuration");
     if (excelFile == null || "".equals(excelFile)) {
+      logger.info("Reader Configuration FAILED -- Excel File not specified ");
       return false;
     }
     File testFile = new File(excelFile);
     if (!testFile.exists()) {
+      logger.info("Reader Configuration FAILED -- Excel File does not exist ");
       return false;
     }
     try {
@@ -171,7 +174,7 @@ public class NetDescAccountsReader implements DataReader {
       newUserRecord.addField("contactId", 6);
       newUserRecord.addField("encryptedPassword", "none");
       writer.save(newUserRecord);
-      
+
       HashMap accounts = new HashMap();
 
       POIFSFileSystem fs =
@@ -190,76 +193,115 @@ public class NetDescAccountsReader implements DataReader {
         HSSFRow row = sheet.getRow(r);
         if (row != null) {
           logger.info("ROW " + row.getRowNum());
-          short firstCellNum = (short) (row.getFirstCellNum() + 1);
+          short firstCellNum = (short) row.getFirstCellNum();
+          logger.info("Processing Account " + getCellValue(row.getCell(firstCellNum)));
 
-          logger.info("Processing Account " + row.getCell(firstCellNum).getStringCellValue());
-          //build the organization
-          Organization thisOrg = new Organization();
-          thisOrg.setOrgId(r);
-          thisOrg.setName(row.getCell(firstCellNum).getStringCellValue());
-          thisOrg.setUrl(row.getCell((short) (firstCellNum + 14)).getStringCellValue());
-          thisOrg.setOwner(userId);
-          thisOrg.setEnteredBy(userId);
-          thisOrg.setModifiedBy(userId);
+          int orgId = r;
+          //Save Account if required
+          if (!accounts.containsKey(getCellValue(row.getCell(firstCellNum)))) {
+            //build the organization
+            Organization thisOrg = new Organization();
+            thisOrg.setOrgId(r);
+            thisOrg.setName(getCellValue(row.getCell(firstCellNum)));
+            thisOrg.setUrl(getCellValue(row.getCell((short) (firstCellNum + 14))));
+            thisOrg.setOwner(userId);
+            thisOrg.setEnteredBy(userId);
+            thisOrg.setModifiedBy(userId);
+            DataRecord thisRecord = mappings.createDataRecord(thisOrg, "insert");
+            accounts.put(thisOrg.getName(), new Integer(r));
+            if (!writer.save(thisRecord)) {
+              processOK = false;
+            }
+          } else {
+            orgId = ((Integer) accounts.get(getCellValue(row.getCell(firstCellNum)))).intValue();
+          }
 
           //build the contact
-          logger.info("Adding Contact " + row.getCell((short) (firstCellNum + 2)).getStringCellValue());
-          Contact cfsContact = new Contact();
-          cfsContact.setId(r);
-          String contactInfo = row.getCell((short) (firstCellNum + 2)).getStringCellValue();
-          if (!"".equals(contactInfo)) {
-            StringTokenizer st = new StringTokenizer(contactInfo, ",");
-            if (st.countTokens() > 1) {
-              cfsContact.setNameLast(st.nextToken());
-              cfsContact.setNameFirst(st.nextToken());
-            } else {
-              StringTokenizer st1 = new StringTokenizer(contactInfo, " ");
-              cfsContact.setNameFirst(st1.nextToken());
-              if (st1.hasMoreTokens()) {
-                cfsContact.setNameLast(st1.nextToken());
+          logger.info("Adding Contact " + getCellValue(row.getCell((short) (firstCellNum + 2))));
+          Contact cfsContact = null;
+          if (!"".equals(getCellValue(row.getCell((short) (firstCellNum + 2))))) {
+            cfsContact = new Contact();
+            cfsContact.setId(r);
+            String contactInfo = getCellValue(row.getCell((short) (firstCellNum + 2)));
+            if (!"".equals(contactInfo)) {
+              StringTokenizer st = new StringTokenizer(contactInfo, ",");
+              if (st.countTokens() > 1) {
+                cfsContact.setNameLast(st.nextToken());
+                cfsContact.setNameFirst(st.nextToken());
+              } else {
+                StringTokenizer st1 = new StringTokenizer(contactInfo, " ");
+                cfsContact.setNameFirst(st1.nextToken());
+                if (st1.hasMoreTokens()) {
+                  cfsContact.setNameLast(st1.nextToken());
+                } else {
+                  cfsContact.setNameLast(cfsContact.getNameFirst());
+                  cfsContact.setNameFirst("");
+                }
               }
             }
+            cfsContact.setTitle(getCellValue(row.getCell((short) (firstCellNum + 13))));
+            cfsContact.setNameSalutation(getCellValue(row.getCell((short) (firstCellNum + 12))));
+            cfsContact.setEnteredBy(userId);
+            cfsContact.setModifiedBy(userId);
+            cfsContact.setOrgId(orgId);
           }
-          cfsContact.setTitle(row.getCell((short) (firstCellNum + 13)).getStringCellValue());
-          cfsContact.setEnteredBy(userId);
-          cfsContact.setModifiedBy(userId);
-          logger.info("Adding Contact Address ");
+
           //build the Contact's Address
-          ContactAddress cfsAddress = new ContactAddress();
-          cfsAddress.setStreetAddressLine1(row.getCell((short) (firstCellNum + 3)).getStringCellValue());
-          cfsAddress.setType(2);
-          cfsAddress.setCity(row.getCell((short) (firstCellNum + 4)).getStringCellValue());
-          cfsAddress.setState(row.getCell((short) (firstCellNum + 5)).getStringCellValue());
-          cfsAddress.setZip(row.getCell((short) (firstCellNum + 6)).getStringCellValue());
-          cfsAddress.setCountry(row.getCell((short) (firstCellNum + 7)).getStringCellValue());
-          cfsAddress.setContactId(r);
-          cfsAddress.setEnteredBy(userId);
-          cfsAddress.setModifiedBy(userId);
+          ContactAddress cfsAddress = null;
+
+          if (!"".equals(getCellValue(row.getCell((short) (firstCellNum + 3))))) {
+            logger.info("Adding Contact Address ");
+            cfsAddress = new ContactAddress();
+            String address = getCellValue(row.getCell((short) (firstCellNum + 3)));
+            StringTokenizer st = new StringTokenizer(address, ",");
+            if (st.hasMoreTokens()) {
+              cfsAddress.setStreetAddressLine1(st.nextToken());
+              if (st.hasMoreTokens()) {
+                cfsAddress.setStreetAddressLine2(st.nextToken());
+              }
+            }
+            cfsAddress.setStreetAddressLine1(getCellValue(row.getCell((short) (firstCellNum + 3))));
+            cfsAddress.setType(2);
+            cfsAddress.setCity(getCellValue(row.getCell((short) (firstCellNum + 4))));
+            cfsAddress.setState(getCellValue(row.getCell((short) (firstCellNum + 5))));
+            cfsAddress.setZip(getCellValue(row.getCell((short) (firstCellNum + 6))));
+            cfsAddress.setCountry(getCellValue(row.getCell((short) (firstCellNum + 7))));
+            cfsAddress.setContactId(r);
+            cfsAddress.setEnteredBy(userId);
+            cfsAddress.setModifiedBy(userId);
+          }
 
           //build Company's Phone Numbers
-          String NetDescComPN = row.getCell((short) (firstCellNum + 9)).getStringCellValue();
-          OrganizationPhoneNumberList cfsCompPhList = new OrganizationPhoneNumberList();
+
+          OrganizationPhoneNumberList cfsCompPhList = null;
+          String NetDescComPN = getCellValue(row.getCell((short) (firstCellNum + 9)));
           if (!"".equals(NetDescComPN)) {
+            cfsCompPhList = new OrganizationPhoneNumberList();
             StringTokenizer st = new StringTokenizer(NetDescComPN, "/");
             while (st.hasMoreTokens()) {
               OrganizationPhoneNumber cfsPhNumber = new OrganizationPhoneNumber();
               cfsPhNumber.setType(1);
               cfsPhNumber.setNumber(st.nextToken());
-              cfsPhNumber.setOrgId(r);
+              cfsPhNumber.setOrgId(orgId);
+              cfsPhNumber.setEnteredBy(userId);
+              cfsPhNumber.setModifiedBy(userId);
               cfsCompPhList.add(cfsPhNumber);
             }
           }
 
           //build Contact's Phone Numbers
-          String NetDescContPN = row.getCell((short) (firstCellNum + 8)).getStringCellValue();
+          logger.info("Contact Phone Numbers ");
           ContactPhoneNumberList cfsContPhList = new ContactPhoneNumberList();
+          String NetDescContPN = getCellValue(row.getCell((short) (firstCellNum + 8)));
           if (!"".equals(NetDescContPN)) {
             StringTokenizer st = new StringTokenizer(NetDescContPN, "/");
             while (st.hasMoreTokens()) {
               ContactPhoneNumber cfsPhNumber = new ContactPhoneNumber();
               cfsPhNumber.setType(1);
               StringTokenizer st1 = new StringTokenizer(st.nextToken(), "x");
-              cfsPhNumber.setNumber(st1.nextToken());
+              String number = st1.nextToken();
+              logger.info("Primary -- " + number);
+              cfsPhNumber.setNumber(number);
               if (st1.hasMoreTokens()) {
                 cfsPhNumber.setExtension(st1.nextToken());
               }
@@ -269,71 +311,83 @@ public class NetDescAccountsReader implements DataReader {
               cfsContPhList.add(cfsPhNumber);
             }
           }
-
           //build Contact's Fax Number
-          ContactPhoneNumber cfsFaxPhNumber = new ContactPhoneNumber();
-          cfsFaxPhNumber.setType(3);
-          cfsFaxPhNumber.setEnteredBy(userId);
-          cfsFaxPhNumber.setModifiedBy(userId);
-          cfsFaxPhNumber.setNumber(row.getCell((short) (firstCellNum + 10)).getStringCellValue());
-          cfsFaxPhNumber.setContactId(r);
-          cfsContPhList.add(cfsFaxPhNumber);
+          ContactPhoneNumber cfsFaxPhNumber = null;
+          if (!"".equals(getCellValue(row.getCell((short) (firstCellNum + 10))))) {
+            cfsFaxPhNumber = new ContactPhoneNumber();
+            cfsFaxPhNumber.setType(3);
+            cfsFaxPhNumber.setEnteredBy(userId);
+            cfsFaxPhNumber.setModifiedBy(userId);
+            String number = getCellValue(row.getCell((short) (firstCellNum + 10)));
+            logger.info("Fax -- " + number);
+            cfsFaxPhNumber.setNumber(number);
+            cfsFaxPhNumber.setContactId(r);
+            cfsContPhList.add(cfsFaxPhNumber);
+          }
 
           //build Contact's Mobile Number
-          ContactPhoneNumber cfsMobilePhNumber = new ContactPhoneNumber();
-          cfsMobilePhNumber.setType(7);
-          cfsMobilePhNumber.setEnteredBy(userId);
-          cfsMobilePhNumber.setModifiedBy(userId);
-          cfsMobilePhNumber.setNumber(row.getCell((short) (firstCellNum + 11)).getStringCellValue());
-          cfsMobilePhNumber.setContactId(r);
-          cfsContPhList.add(cfsFaxPhNumber);
+
+          ContactPhoneNumber cfsMobilePhNumber = null;
+          if (!"".equals(getCellValue(row.getCell((short) (firstCellNum + 11))))) {
+            cfsMobilePhNumber = new ContactPhoneNumber();
+            cfsMobilePhNumber.setType(7);
+            cfsMobilePhNumber.setEnteredBy(userId);
+            cfsMobilePhNumber.setModifiedBy(userId);
+            String number = getCellValue(row.getCell((short) (firstCellNum + 11)));
+            logger.info("Mobile -- " + number);
+            cfsMobilePhNumber.setNumber(number);
+            cfsMobilePhNumber.setContactId(r);
+            cfsContPhList.add(cfsMobilePhNumber);
+          }
 
           //build Contact's Email Addresse's
-          ContactEmailAddress cfsEmailAddress = new ContactEmailAddress();
-          cfsEmailAddress.setType(2);
-          cfsEmailAddress.setEmail(row.getCell((short) (firstCellNum + 12)).getStringCellValue());
-          cfsEmailAddress.setContactId(r);
-          cfsEmailAddress.setEnteredBy(userId);
-          cfsEmailAddress.setModifiedBy(userId);
+          logger.info("EmailAddress -- " + getCellValue(row.getCell((short) (firstCellNum + 15))));
+          ContactEmailAddress cfsEmailAddress = null;
+          if (!"".equals(getCellValue(row.getCell((short) (firstCellNum + 15))))) {
+            cfsEmailAddress = new ContactEmailAddress();
+            cfsEmailAddress.setType(2);
+            cfsEmailAddress.setEmail(getCellValue(row.getCell((short) (firstCellNum + 15))));
+            cfsEmailAddress.setContactId(r);
+            cfsEmailAddress.setEnteredBy(userId);
+            cfsEmailAddress.setModifiedBy(userId);
+          }
 
-          logger.info("ImportAccounts --> Saving Contact " + cfsContact.getId());
+          logger.info("ImportAccounts --> Saving Contact ");
 
-          //Save Account if required
-          if (!accounts.containsKey(row.getCell(firstCellNum).getStringCellValue())) {
-            DataRecord thisRecord = mappings.createDataRecord(thisOrg, "insert");
-            accounts.put(thisOrg.getName(), new Integer(0));
-            if (!writer.save(thisRecord)) {
+          //Save Contact Info inlcuding address, phone numbers only if contact exists.
+          if (cfsContact != null) {
+            DataRecord contRecord = mappings.createDataRecord(cfsContact, "insert");
+            if (!writer.save(contRecord)) {
               processOK = false;
             }
-          } else {
-            Integer count = (Integer) accounts.get(row.getCell(firstCellNum).getStringCellValue());
-            accounts.remove(row.getCell(firstCellNum).getStringCellValue());
-            accounts.put(row.getCell(firstCellNum).getStringCellValue(), new Integer(count.intValue() + 1));
-          }
 
-          //Save Contact Info
-          DataRecord contRecord = mappings.createDataRecord(cfsContact, "insert");
-          if (!writer.save(contRecord)) {
-            processOK = false;
-          }
+            //Save Contact Address
+            if (cfsAddress != null) {
+              DataRecord addRecord = mappings.createDataRecord(cfsAddress, "insert");
+              if (!writer.save(addRecord)) {
+                processOK = false;
+              }
+            }
 
-          //Save Contact Address
-          DataRecord addRecord = mappings.createDataRecord(cfsAddress, "insert");
-          if (!writer.save(addRecord)) {
-            processOK = false;
+            //Save Contact Fax, Mobile & Phone Numbers
+            if (cfsContPhList != null) {
+              processOK = mappings.saveList(writer, cfsContPhList, "insert");
+            }
+
+            //Save Contact EmailAddress
+            if (cfsEmailAddress != null) {
+              DataRecord emailRecord = mappings.createDataRecord(cfsEmailAddress, "insert");
+              if (!writer.save(emailRecord)) {
+                processOK = false;
+              }
+            }
           }
 
           //Save Company Phone Numbers
-          processOK = mappings.saveList(writer, cfsCompPhList, "insert");
-
-          //Save Contact Fax, Mobile & Phone Numbers
-          processOK = mappings.saveList(writer, cfsContPhList, "insert");
-
-          //Save Contact EmailAddress
-          DataRecord emailRecord = mappings.createDataRecord(cfsEmailAddress, "insert");
-          if (!writer.save(emailRecord)) {
-            processOK = false;
+          if (cfsCompPhList != null) {
+            processOK = mappings.saveList(writer, cfsCompPhList, "insert");
           }
+
           if (!processOK) {
             return false;
           }
@@ -344,6 +398,35 @@ public class NetDescAccountsReader implements DataReader {
       return false;
     }
     return true;
+  }
+
+
+  /**
+   *  Gets the Excel cell value depending on the Type of the cell.
+   *
+   *@param  cell  Description of the Parameter
+   *@return       The cellValue value
+   */
+  private String getCellValue(HSSFCell cell) {
+    String value = null;
+    switch (cell.getCellType()) {
+        case HSSFCell.CELL_TYPE_BLANK:
+          value = "";
+          break;
+        case HSSFCell.CELL_TYPE_BOOLEAN:
+          value = Boolean.toString(cell.getBooleanCellValue());
+          break;
+        case HSSFCell.CELL_TYPE_NUMERIC:
+          value = Double.toString(cell.getNumericCellValue()).trim();
+          break;
+        case HSSFCell.CELL_TYPE_STRING:
+          value = cell.getStringCellValue().trim();
+          break;
+        default:
+          value = "";
+          break;
+    }
+    return value;
   }
 
 }
