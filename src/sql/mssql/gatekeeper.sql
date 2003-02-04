6073ce2a910cd55047f8ee0e52505d0451d1b1fe
mrajkowski <mrajkowski@concursive.com>
2003-02-04 21:13:58 +0000

    added crontab events tables

--- src/sql/mssql/gatekeeper.sql
+++ src/sql/mssql/gatekeeper.sql
@@ -20,3 +20,26 @@ CREATE TABLE sites (
   enabled BIT NOT NULL DEFAULT 0
 );
 
+CREATE TABLE events (
+  event_id INT IDENTITY PRIMARY KEY,
+  second VARCHAR(64) DEFAULT '0',
+  minute VARCHAR(64) DEFAULT '*', 
+  hour VARCHAR(64) DEFAULT '*',
+  dayofmonth VARCHAR(64) DEFAULT '*',
+  month VARCHAR(64) DEFAULT '*', 
+  dayofweek VARCHAR(64) DEFAULT '*',
+  year VARCHAR(64) DEFAULT '*',
+  task VARCHAR(255),
+  extrainfo VARCHAR(255),
+  businessDays VARCHAR(6) DEFAULT 'true',
+  enabled BIT DEFAULT 0,
+  entered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE events_log (
+  log_id SERIAL INTEGER PRIMARY KEY,
+  event_id INTEGER DEFAULT NOT NULL,
+  entered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status INT,
+  message TEXT
+);
--- src/sql/postgresql/new_gatekeeper.sql
+++ src/sql/postgresql/new_gatekeeper.sql
@@ -19,3 +19,30 @@ CREATE TABLE sites (
 
 GRANT ALL ON sites TO gatekeeper;
 
+CREATE TABLE events (
+  event_id SERIAL PRIMARY KEY,
+  second VARCHAR(64) DEFAULT '0',
+  minute VARCHAR(64) DEFAULT '*', 
+  hour VARCHAR(64) DEFAULT '*',
+  dayofmonth VARCHAR(64) DEFAULT '*',
+  month VARCHAR(64) DEFAULT '*', 
+  dayofweek VARCHAR(64) DEFAULT '*',
+  year VARCHAR(64) DEFAULT '*',
+  task VARCHAR(255),
+  extrainfo VARCHAR(255),
+  businessDays VARCHAR(6) DEFAULT 'true',
+  enabled BOOLEAN DEFAULT false,
+  entered TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+GRANT ALL ON events TO gatekeeper;
+
+CREATE TABLE events_log (
+  log_id SERIAL PRIMARY KEY,
+  event_id INTEGER NOT NULL,
+  entered TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status INT,
+  message TEXT
+);
+
+GRANT ALL ON events_log TO gatekeeper;
