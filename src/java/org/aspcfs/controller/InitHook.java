91c3dcd9e1556e473f77c189f7aaca9e3567ccf3
mrajkowski <mrajkowski@concursive.com>
2003-02-04 21:13:09 +0000

    Initialize jcrontab

--- src/java/org/aspcfs/controller/InitHook.java
+++ src/java/org/aspcfs/controller/InitHook.java
@@ -7,6 +7,9 @@ import com.darkhorseventures.database.*;
 import org.aspcfs.utils.web.CustomFormList;
 import java.sql.*;
 import java.util.Hashtable;
+import java.util.Properties;
+import org.jcrontab.Crontab;
+import org.aspcfs.utils.StringUtils;
 
 /**
  *  Code that is initialized when the ServletController starts.
@@ -16,16 +19,18 @@ import java.util.Hashtable;
  *@version    $Id$
  */
 public class InitHook implements ControllerInitHook {
-
+  public static final String fs = System.getProperty("file.separator");
+  
   /**
-   *  When the ServletController is initialized, this code creates a
-   *  ConnectionPool and maps it to the Application scope.
+   *  When the ServletController is initialized, this code maps init-params
+   *  to the Context or Application scope.
    *
    *@param  config  Description of Parameter
    *@return         Description of the Returned Value
    *@since          1.1
    */
   public String executeControllerInit(ServletConfig config) {
+    System.out.println("InitHook-> Executing");
     ServletContext context = config.getServletContext();
 
     InitHook.addAttribute(config, context, "SiteCode", "GATEKEEPER.APPCODE");
@@ -67,7 +72,38 @@ public class InitHook implements ControllerInitHook {
       context.setAttribute("MailServer", "127.0.0.1");
       System.setProperty("MailServer", "127.0.0.1");
     }
-
+    
+    
+    //Start the cron last
+    if ("true".equals(config.getInitParameter("CRON.ENABLED"))) {
+      try {
+        System.out.println("InitHook-> Starting CRON");
+        Class cl = Class.forName("org.aspcfs.apps.notifier.Notifier");
+        Crontab crontab = Crontab.getInstance();
+        Properties jcronProperties = new Properties();
+        //jcron settings
+        jcronProperties.setProperty("org.jcrontab.Crontab.refreshFrequency", "1");
+/*
+        //jcron datasource for cron entries
+        jcronProperties.setProperty("org.jcrontab.data.datasource", "org.jcrontab.data.FileSource");
+        jcronProperties.setProperty("org.jcrontab.data.file", context.getRealPath("/") + "WEB-INF" + fs + "crontab");
+*/
+        //Use a database instead...
+        jcronProperties.setProperty("org.jcrontab.data.datasource", "org.jcrontab.data.GenericSQLSource");
+        jcronProperties.setProperty("org.jcrontab.data.GenericSQLSource.driver", StringUtils.toString((String)context.getAttribute("GKDRIVER")));
+        jcronProperties.setProperty("org.jcrontab.data.GenericSQLSource.url", StringUtils.toString((String)context.getAttribute("GKHOST")));
+        jcronProperties.setProperty("org.jcrontab.data.GenericSQLSource.username", StringUtils.toString((String)context.getAttribute("GKUSER")));
+        jcronProperties.setProperty("org.jcrontab.data.GenericSQLSource.password", StringUtils.toString((String)context.getAttribute("GKUSERPW")));
+        
+        //jcron logger -- TODO: implement a database logger
+        jcronProperties.setProperty("org.jcrontab.log.Logger", "org.jcrontab.log.DebugLogger");
+        crontab.init(jcronProperties);
+        context.setAttribute("Crontab", crontab);
+      } catch (Exception e) {
+        System.err.println(e.toString());
+      }
+    }
+    
     return null;
   }
 
