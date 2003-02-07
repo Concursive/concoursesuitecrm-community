78d692507d7c944a603a9d66d0829741b2dd5375
akhi_m <akhi_m@concursive.com>
2003-02-07 14:41:36 +0000

    formatting

--- src/java/org/aspcfs/modules/contacts/base/ContactPhoneNumber.java
+++ src/java/org/aspcfs/modules/contacts/base/ContactPhoneNumber.java
@@ -36,11 +36,19 @@ public class ContactPhoneNumber extends PhoneNumber {
     isContact = true;
     buildRecord(rs);
   }
-  
-  
+
+
+  /**
+   *  Constructor for the ContactPhoneNumber object
+   *
+   *@param  db                Description of the Parameter
+   *@param  phoneNumberId     Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
   public ContactPhoneNumber(Connection db, int phoneNumberId) throws SQLException {
-          queryRecord(db, phoneNumberId);
+    queryRecord(db, phoneNumberId);
   }
+
 
   /**
    *  Constructor for the ContactPhoneNumber object
@@ -51,9 +59,17 @@ public class ContactPhoneNumber extends PhoneNumber {
    *@since
    */
   public ContactPhoneNumber(Connection db, String phoneNumberId) throws SQLException {
-          queryRecord(db, Integer.parseInt(phoneNumberId));
+    queryRecord(db, Integer.parseInt(phoneNumberId));
   }
-          
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@param  phoneNumberId     Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
   public void queryRecord(Connection db, int phoneNumberId) throws SQLException {
     isContact = true;
     if (phoneNumberId <= 0) {
@@ -64,10 +80,10 @@ public class ContactPhoneNumber extends PhoneNumber {
     ResultSet rs = null;
     StringBuffer sql = new StringBuffer();
     sql.append(
-      "SELECT * " +
-      "FROM contact_phone p, lookup_contactphone_types l " +
-      "WHERE p.phone_type = l.code " +
-      "AND phone_id = " + phoneNumberId + " ");
+        "SELECT * " +
+        "FROM contact_phone p, lookup_contactphone_types l " +
+        "WHERE p.phone_type = l.code " +
+        "AND phone_id = " + phoneNumberId + " ");
     st = db.createStatement();
     rs = st.executeQuery(sql.toString());
     if (rs.next()) {
@@ -96,12 +112,12 @@ public class ContactPhoneNumber extends PhoneNumber {
   public void process(Connection db, int contactId, int enteredBy, int modifiedBy) throws SQLException {
     if (this.getEnabled() == true) {
       if (this.getId() == -1) {
-	this.setContactId(contactId);
-	this.setEnteredBy(enteredBy);
-	this.setModifiedBy(modifiedBy);
+        this.setContactId(contactId);
+        this.setEnteredBy(enteredBy);
+        this.setModifiedBy(modifiedBy);
         this.insert(db);
       } else {
-	this.setModifiedBy(modifiedBy);
+        this.setModifiedBy(modifiedBy);
         this.update(db, modifiedBy);
       }
     } else {
@@ -114,35 +130,42 @@ public class ContactPhoneNumber extends PhoneNumber {
    *  Inserts this phone number into the contact database
    *
    *@param  db                Description of Parameter
-   *@param  contactId         Description of Parameter
-   *@param  enteredBy         Description of Parameter
    *@exception  SQLException  Description of Exception
    *@since
    */
-   
-   public void insert(Connection db) throws SQLException {
-           insert(db, this.getContactId(), this.getEnteredBy());
-   }
-   
+
+  public void insert(Connection db) throws SQLException {
+    insert(db, this.getContactId(), this.getEnteredBy());
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@param  contactId         Description of the Parameter
+   *@param  enteredBy         Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
   public void insert(Connection db, int contactId, int enteredBy) throws SQLException {
     StringBuffer sql = new StringBuffer();
     sql.append("INSERT INTO contact_phone " +
         "(contact_id, phone_type, number, extension, ");
-                if (this.getEntered() != null) {
-                        sql.append("entered, ");
-                }
-                if (this.getModified() != null) {
-                        sql.append("modified, ");
-                }
+    if (this.getEntered() != null) {
+      sql.append("entered, ");
+    }
+    if (this.getModified() != null) {
+      sql.append("modified, ");
+    }
     sql.append("enteredBy, modifiedBy ) ");
     sql.append("VALUES (?, ?, ?, ?, ");
-                if (this.getEntered() != null) {
-                        sql.append("?, ");
-                }
-                if (this.getModified() != null) {
-                        sql.append("?, ");
-                }    
-    sql.append("?, ?) ");                
+    if (this.getEntered() != null) {
+      sql.append("?, ");
+    }
+    if (this.getModified() != null) {
+      sql.append("?, ");
+    }
+    sql.append("?, ?) ");
     int i = 0;
     PreparedStatement pst = db.prepareStatement(sql.toString());
     if (contactId > -1) {
@@ -157,14 +180,14 @@ public class ContactPhoneNumber extends PhoneNumber {
     }
     pst.setString(++i, this.getNumber());
     pst.setString(++i, this.getExtension());
-        if (this.getEntered() != null) {
-                pst.setTimestamp(++i, this.getEntered());
-        }
-        if (this.getModified() != null) {
-                pst.setTimestamp(++i, this.getModified());
-        }
-      pst.setInt(++i, this.getEnteredBy());
-      pst.setInt(++i, this.getModifiedBy());
+    if (this.getEntered() != null) {
+      pst.setTimestamp(++i, this.getEntered());
+    }
+    if (this.getModified() != null) {
+      pst.setTimestamp(++i, this.getModified());
+    }
+    pst.setInt(++i, this.getEnteredBy());
+    pst.setInt(++i, this.getModifiedBy());
     pst.execute();
     pst.close();
 
