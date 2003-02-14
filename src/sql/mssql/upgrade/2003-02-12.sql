a9321b8414bb66eaffa46b774587a640c4c8b984
mrajkowski <mrajkowski@concursive.com>
2003-02-14 16:28:09 +0000

    Changed Access Contacts & Resources to Access General Contacts

--- src/sql/mssql/upgrade/2003-02-12.sql
+++ src/sql/mssql/upgrade/2003-02-12.sql
@@ -3,3 +3,6 @@ ALTER TABLE lookup_lists_lookup ADD category_id INT;
 UPDATE lookup_lists_lookup SET category_id = module_id;
 ALTER TABLE lookup_lists_lookup ALTER COLUMN category_id INT NOT NULL;
 
+UPDATE permission 
+SET description = 'Access to General Contacts module' 
+WHERE description = 'Access to Contacts & Resources module';
--- src/sql/postgresql/upgrade/2003-02-12.sql
+++ src/sql/postgresql/upgrade/2003-02-12.sql
@@ -3,3 +3,6 @@ ALTER TABLE lookup_lists_lookup ADD COLUMN category_id INT;
 UPDATE lookup_lists_lookup SET category_id = module_id;
 ALTER TABLE lookup_lists_lookup ADD CONSTRAINT notnullchk CHECK (category_id IS NOT NULL);
 
+UPDATE permission 
+SET description = 'Access to General Contacts module' 
+WHERE description = 'Access to Contacts & Resources module';
