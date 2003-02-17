f91bda13219feacea062a5f855f27832d8a6516a
mrajkowski <mrajkowski@concursive.com>
2003-02-17 14:38:42 +0000

    Implemented pagedListInfo in query

--- src/java/org/aspcfs/modules/media/autoguide/base/AdRunList.java
+++ src/java/org/aspcfs/modules/media/autoguide/base/AdRunList.java
@@ -6,13 +6,14 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.sql.*;
 import org.aspcfs.utils.DatabaseUtils;
+import org.aspcfs.utils.web.PagedListInfo;
 import org.aspcfs.modules.base.Constants;
 import javax.servlet.http.*;
 
 /**
  *  Represents a list of ads (AdRun)
  *
- *@author     matt
+ *@author     matt rajkowski
  *@created    May 17, 2002
  *@version    $Id$
  */
@@ -25,6 +26,7 @@ public class AdRunList extends ArrayList {
   private int syncType = Constants.NO_SYNC;
   private int inventoryId = -1;
   private boolean incompleteOnly = false;
+  private PagedListInfo pagedListInfo = null;
 
 
   /**
@@ -181,6 +183,17 @@ public class AdRunList extends ArrayList {
 
 
   /**
+   *  Sets the pagedListInfo attribute of the AdRunList object
+   *
+   *@param  tmp  The new pagedListInfo value
+   */
+  public void setPagedListInfo(PagedListInfo tmp) {
+    this.pagedListInfo = tmp;
+  }
+
+
+
+  /**
    *  Gets the tableName attribute of the AdRunList object
    *
    *@return    The tableName value
@@ -217,6 +230,16 @@ public class AdRunList extends ArrayList {
    */
   public int getAccountInventoryId() {
     return inventoryId;
+  }
+
+
+  /**
+   *  Gets the pagedListInfo attribute of the AdRunList object
+   *
+   *@return    The pagedListInfo value
+   */
+  public PagedListInfo getPagedListInfo() {
+    return pagedListInfo;
   }
 
 
@@ -285,7 +308,14 @@ public class AdRunList extends ArrayList {
   public void buildList(Connection db) throws SQLException {
     PreparedStatement pst = null;
     ResultSet rs = queryList(db, pst);
+    int count = 0;
     while (rs.next()) {
+      if (pagedListInfo != null && pagedListInfo.getItemsPerPage() > 0 &&
+          DatabaseUtils.getType(db) == DatabaseUtils.MSSQL &&
+          count >= pagedListInfo.getItemsPerPage()) {
+        break;
+      }
+      ++count;
       AdRun thisAdRun = this.getObject(rs);
       this.add(thisAdRun);
     }
@@ -307,22 +337,53 @@ public class AdRunList extends ArrayList {
   public ResultSet queryList(Connection db, PreparedStatement pst) throws SQLException {
     ResultSet rs = null;
     int items = -1;
+    StringBuffer sqlSelect = new StringBuffer();
+    StringBuffer sqlCount = new StringBuffer();
+    StringBuffer sqlFilter = new StringBuffer();
+    StringBuffer sqlOrder = new StringBuffer();
 
-    StringBuffer sql = new StringBuffer();
-    sql.append(
-        "SELECT ad.ad_run_id, ad.inventory_id, " +
+    sqlCount.append(
+        "SELECT COUNT(*) AS recordcount " +
+        "FROM autoguide_ad_run ad, autoguide_ad_run_types adtype " +
+        "WHERE ad.ad_run_id > -1 AND ad.ad_type = adtype.code ");
+    createFilter(sqlFilter);
+    if (pagedListInfo != null) {
+      //Get the total number of records matching filter
+      pst = db.prepareStatement(sqlCount.toString() + sqlFilter.toString());
+      items = prepareFilter(pst);
+      rs = pst.executeQuery();
+      if (rs.next()) {
+        int maxRecords = rs.getInt("recordcount");
+        pagedListInfo.setMaxRecords(maxRecords);
+      }
+      pst.close();
+      rs.close();
+      //Determine column to sort by
+      pagedListInfo.setDefaultSort("run_date", null);
+      pagedListInfo.appendSqlTail(db, sqlOrder);
+    } else {
+      sqlOrder.append("ORDER BY run_date ");
+    }
+    //Need to build a base SQL statement for returning records
+    if (pagedListInfo != null) {
+      pagedListInfo.appendSqlSelectHead(db, sqlSelect);
+    } else {
+      sqlSelect.append("SELECT ");
+    }
+    sqlSelect.append(
+        "ad.ad_run_id, ad.inventory_id, " +
         "ad.run_date, ad.ad_type, ad.include_photo, complete_date, completedby, " +
         "ad.entered, ad.enteredby, " +
         "ad.modified, ad.modifiedby, " +
         "adtype.description " +
         "FROM autoguide_ad_run ad, autoguide_ad_run_types adtype " +
         "WHERE ad.ad_run_id > -1 AND ad.ad_type = adtype.code ");
-    createFilter(sql);
-    sql.append("ORDER BY run_date ");
-    //sql.append("ORDER BY inventory_id, start_date DESC ");
-    pst = db.prepareStatement(sql.toString());
+    pst = db.prepareStatement(sqlSelect.toString() + sqlFilter.toString() + sqlOrder.toString());
     items = prepareFilter(pst);
     rs = pst.executeQuery();
+    if (pagedListInfo != null) {
+      pagedListInfo.doManualOffset(db, rs);
+    }
     return rs;
   }
 
