1779cf44ef778ebdf84f25399ac74244dcb5116a
mrajkowski <mrajkowski@concursive.com>
2003-02-17 21:10:31 +0000

    FIX: tokenizer bug

--- src/java/org/aspcfs/modules/media/autoguide/utils/ParseKBB.java
+++ src/java/org/aspcfs/modules/media/autoguide/utils/ParseKBB.java
@@ -8,13 +8,25 @@ import com.darkhorseventures.database.*;
 import java.util.logging.*;
 
 /**
- *  Description of the Class
+ *  This application is responsible for processing a .csv textfile and importing
+ *  into a CFS database.<p>
  *
- *@author     matt
+ *  The file is in either format:<br>
+ *  model,make,year1,year2,year3<br>
+ *  model,make,year1 year2 year3
+ *
+ *@author     matt rajkowski
  *@created    April 18, 2002
  *@version    $Id$
  */
 public class ParseKBB {
+
+  private String file = null;
+  private String driver = null;
+  private String url = null;
+  private String user = null;
+  private String pass = null;
+
 
   /**
    *  Constructor for the ParseKBB object
@@ -29,23 +41,127 @@ public class ParseKBB {
    */
   public static void main(String[] args) {
     ParseKBB thisApp = new ParseKBB();
-    String fileName = null;
-    if (args.length > 0) {
-      fileName = args[0];
-    } else {
-      fileName = "autoguide.csv";
-    }
-    thisApp.process(fileName);
+    thisApp.setFile(System.getProperty("file"));
+    thisApp.setDriver(System.getProperty("driver"));
+    thisApp.setUrl(System.getProperty("url"));
+    thisApp.setUser(System.getProperty("user"));
+    thisApp.setPass(System.getProperty("pass"));
+    thisApp.process();
+    System.exit(0);
   }
 
 
   /**
-   *  Description of the Method
+   *  Sets the file attribute of the ParseKBB object
    *
-   *@param  fileName  Description of the Parameter
+   *@param  tmp  The new file value
    */
-  public void process(String fileName) {
+  public void setFile(String tmp) {
+    this.file = tmp;
+  }
+
+
+  /**
+   *  Sets the driver attribute of the ParseKBB object
+   *
+   *@param  tmp  The new driver value
+   */
+  public void setDriver(String tmp) {
+    this.driver = tmp;
+  }
+
+
+  /**
+   *  Sets the url attribute of the ParseKBB object
+   *
+   *@param  tmp  The new url value
+   */
+  public void setUrl(String tmp) {
+    this.url = tmp;
+  }
+
+
+  /**
+   *  Sets the user attribute of the ParseKBB object
+   *
+   *@param  tmp  The new user value
+   */
+  public void setUser(String tmp) {
+    this.user = tmp;
+  }
+
+
+  /**
+   *  Sets the pass attribute of the ParseKBB object
+   *
+   *@param  tmp  The new pass value
+   */
+  public void setPass(String tmp) {
+    this.pass = tmp;
+  }
+
+
+  /**
+   *  Gets the file attribute of the ParseKBB object
+   *
+   *@return    The file value
+   */
+  public String getFile() {
+    return file;
+  }
+
+
+  /**
+   *  Gets the driver attribute of the ParseKBB object
+   *
+   *@return    The driver value
+   */
+  public String getDriver() {
+    return driver;
+  }
+
+
+  /**
+   *  Gets the url attribute of the ParseKBB object
+   *
+   *@return    The url value
+   */
+  public String getUrl() {
+    return url;
+  }
+
+
+  /**
+   *  Gets the user attribute of the ParseKBB object
+   *
+   *@return    The user value
+   */
+  public String getUser() {
+    return user;
+  }
+
+
+  /**
+   *  Gets the pass attribute of the ParseKBB object
+   *
+   *@return    The pass value
+   */
+  public String getPass() {
+    return pass;
+  }
+
+
+  /**
+   *  Connects to the destination database and reads the file
+   *
+   */
+  public void process() {
     Logger logger = Logger.getLogger("org.aspcfs.modules.media.autoguide");
+    if (file == null || url == null) {
+      logger.info("Not configured");
+      return;
+    }
+    
     java.util.Date startDate = new java.util.Date();
     long startMs = System.currentTimeMillis();
     ConnectionPool sqlDriver = null;
@@ -59,16 +175,12 @@ public class ParseKBB {
     try {
       sqlDriver.setForceClose(false);
       sqlDriver.setMaxConnections(1);
-      ConnectionElement thisElement = new ConnectionElement(
-          "jdbc:microsoft:sqlserver://127.0.0.1:1433;DatabaseName=cdb_cfs;SelectMethod=cursor",
-          "dhv_appuser",
-          "@pus3r");
-      thisElement.setDriver("com.microsoft.jdbc.sqlserver.SQLServerDriver");
+      ConnectionElement thisElement = new ConnectionElement(url, user, pass);
+      thisElement.setDriver(driver);
       db = sqlDriver.getConnection(thisElement);
-
       String line = null;
       try {
-        BufferedReader in = new BufferedReader(new FileReader(fileName));
+        BufferedReader in = new BufferedReader(new FileReader(file));
         while ((line = in.readLine()) != null) {
           processLine(db, line);
         }
@@ -76,7 +188,6 @@ public class ParseKBB {
       } catch (IOException ex) {
         logger.log(Level.SEVERE, "Error reading file", ex);
       }
-
       sqlDriver.free(db);
     } catch (SQLException e) {
       logger.log(Level.SEVERE, "SQL Error", e);
@@ -99,6 +210,7 @@ public class ParseKBB {
    *@exception  SQLException  Description of Exception
    */
   private void processLine(Connection db, String line) throws SQLException {
+    Logger logger = Logger.getLogger("org.aspcfs.modules.media.autoguide");
     StringTokenizer st = new StringTokenizer(line, ",");
     String modelText = st.nextToken().trim();
     String makeText = st.nextToken().trim();
@@ -106,26 +218,44 @@ public class ParseKBB {
     Make make = new Make(makeText);
     if (!make.exists(db)) {
       make.insert(db);
-      Logger logger = Logger.getLogger("org.aspcfs.modules.media.autoguide");
       logger.fine("Inserting Make-> " + make.getName());
     }
 
     Model model = new Model(modelText);
     model.setMakeId(make.getId());
     if (!model.exists(db)) {
-      Logger logger = Logger.getLogger("org.aspcfs.modules.media.autoguide");
       logger.fine("Inserting Model-> " + model.getName());
-      //System.out.println("Inserting-> " + make.getName() + " | " + model.getName());
       model.insert(db);
+    }
 
-      while (st.hasMoreTokens()) {
-        int year = Integer.parseInt(st.nextToken());
+    //Process the years, could be in one of two formats
+    //Ex. 97,98,99 or 97 98 99
+    while (st.hasMoreTokens()) {
+      String yearString = st.nextToken().trim();
+      if (yearString.indexOf(" ") > -1) {
+        //If multiple years are listed, process them all
+        //Ex. 97 98 99
+        StringTokenizer multipleYears = new StringTokenizer(yearString, " ");
+        while (multipleYears.hasMoreTokens()) {
+          String thisYear = multipleYears.nextToken().trim();
+          int year = Integer.parseInt(thisYear);
+          Vehicle thisVehicle = new Vehicle(year, make.getId(), model.getId());
+          int id = thisVehicle.generateId(db);
+          if (id == -1) {
+            thisVehicle.insert(db);
+            logger.fine("Inserting Year-> " + thisVehicle.getYear());
+          }
+        }
+      } else {
+        //Single year in this token: 97
+        int year = Integer.parseInt(yearString);
         Vehicle thisVehicle = new Vehicle(year, make.getId(), model.getId());
-        logger.fine("Inserting Year-> " + thisVehicle.getYear());
-        //System.out.println("         -> " + thisVehicle.getYear());
-        thisVehicle.insert(db);
+        int id = thisVehicle.generateId(db);
+        if (id == -1) {
+          thisVehicle.insert(db);
+          logger.fine("Inserting Year-> " + thisVehicle.getYear());
+        }
       }
-
     }
   }
 }
