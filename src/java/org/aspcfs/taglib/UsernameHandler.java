eba7c29339378b463620b6c5a0a777bbcc21fcf6
mrajkowski <mrajkowski@concursive.com>
2003-03-14 16:19:18 +0000

    NEW: Added lastFirst attribute to return the name by last name, first name

--- src/java/org/aspcfs/taglib/UsernameHandler.java
+++ src/java/org/aspcfs/taglib/UsernameHandler.java
@@ -13,7 +13,7 @@ import java.util.*;
  *  This Class evaluates a User ID and returns a Contact record from application
  *  scope.
  *
- *@author     matt
+ *@author     matt rajkowski
  *@created    December 14, 2001
  *@version    $Id: UsernameHandler.java,v 1.3 2002/12/23 16:12:28 mrajkowski Exp
  *      $
@@ -21,6 +21,7 @@ import java.util.*;
 public class UsernameHandler extends TagSupport {
 
   private int userId = -1;
+  private boolean lastFirst = false;
 
 
   /**
@@ -45,6 +46,16 @@ public class UsernameHandler extends TagSupport {
 
 
   /**
+   *  Sets the lastFirst attribute of the UsernameHandler object
+   *
+   *@param  tmp  The new lastFirst value
+   */
+  public void setLastFirst(String tmp) {
+    this.lastFirst = "true".equals(tmp);
+  }
+
+
+  /**
    *  Description of the Method
    *
    *@return                   Description of the Returned Value
@@ -53,7 +64,6 @@ public class UsernameHandler extends TagSupport {
    */
   public int doStartTag() throws JspException {
     try {
-
       ConnectionElement ce = (ConnectionElement) pageContext.getSession().getAttribute("ConnectionElement");
       if (ce == null) {
         System.out.println("UsernameHandler-> ConnectionElement is null");
@@ -65,7 +75,11 @@ public class UsernameHandler extends TagSupport {
       User thisUser = systemStatus.getUser(userId);
       if (thisUser != null) {
         Contact thisContact = thisUser.getContact();
-        this.pageContext.getOut().write(thisContact.getNameFirstLast());
+        if (lastFirst) {
+          this.pageContext.getOut().write(thisContact.getNameLastFirst());
+        } else {
+          this.pageContext.getOut().write(thisContact.getNameFirstLast());
+        }
       } else {
         System.out.println("UsernameHandler-> User is null");
         this.pageContext.getOut().write("");
