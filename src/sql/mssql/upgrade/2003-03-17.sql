f6d3e63b9b9b28c5fc66b373db314df40ab04480
mrajkowski <mrajkowski@concursive.com>
2003-03-17 19:00:15 +0000

    CHANGE: By default, disable miner and setting profile info

---
+++ src/sql/mssql/upgrade/2003-03-17.sql
@@ -0,0 +1,3 @@
+/* Turn these off because they are not fully functional */
+UPDATE permission SET enabled = false WHERE permission = 'myhomepage-miner';
+UPDATE permission SET enabled = false WHERE permission = 'myhomepage-profile-settings';
