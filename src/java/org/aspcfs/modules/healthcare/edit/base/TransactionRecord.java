e46580dd8bd58a1ddeb3c63222f54d79aa8a30e8
mrajkowski <mrajkowski@concursive.com>
2003-03-24 19:26:20 +0000

    CHANGED: removed HTTP request processing, added a couple of redundant methods to support the XML import and parsing

--- src/java/org/aspcfs/modules/healthcare/edit/base/TransactionRecord.java
+++ src/java/org/aspcfs/modules/healthcare/edit/base/TransactionRecord.java
@@ -21,7 +21,8 @@ import javax.servlet.http.*;
  *
  *@author     chris
  *@created    February 6, 2003
- *@version    $Id$
+ *@version    $Id: TransactionRecord.java,v 1.3 2003/03/21 22:30:51 mrajkowski
+ *      Exp $
  */
 public class TransactionRecord extends GenericBean {
 
@@ -57,68 +58,6 @@ public class TransactionRecord extends GenericBean {
    */
   public TransactionRecord(ResultSet rs) throws SQLException {
     buildRecord(rs);
-  }
-
-
-  /**
-   *  Builds out a transaction record based on the request parameters. 11 values
-   *  are sent in a comma-separated list, each value surrounded by single-quotes
-   *
-   *@param  request  Description of the Parameter
-   *@return          Description of the Return Value
-   */
-  public boolean buildRecord(HttpServletRequest request) {
-    String params = null;
-    String currentValue = null;
-    String[] values = new String[11];
-    int i = 0;
-    int j = 0;
-
-    if (request.getParameter("values") == null) {
-      errors.put("parametersError", "Parameters cannot be blank");
-      return false;
-    }
-
-    params = request.getParameter("values");
-
-    StringTokenizer st = new StringTokenizer(params, ",");
-
-    for (j = 0; j < 11; j++) {
-      values[j] = null;
-    }
-
-    while (st.hasMoreTokens()) {
-      currentValue = st.nextToken().trim();
-      //strip away the single-quotes
-      currentValue = currentValue.substring(1, currentValue.length() - 1);
-      if (currentValue.length() > 0) {
-        values[i] = currentValue;
-      }
-      i++;
-    }
-
-    //must have 11 parameters
-    if (i < 11) {
-      errors.put("parametersError", "Transaction must have 11 parameters specified");
-      return false;
-    }
-
-    //set the values based on their order in the request string
-    taxId = values[0];
-    licenseNumber = values[1];
-    npi = values[2];
-    providerId = values[3];
-    nameLast = values[4];
-    nameFirst = values[5];
-    payerId = values[6];
-    type = values[7].toUpperCase();
-    transactionId = values[8];
-    transactionDate = values[9];
-    this.setPerformedDate(transactionDate);
-    transactionTime = values[10];
-    this.setPerformed(transactionDate + transactionTime);
-
-    return true;
   }
 
 
@@ -163,6 +102,8 @@ public class TransactionRecord extends GenericBean {
    */
   public void setTransactionDate(String transactionDate) {
     this.transactionDate = transactionDate;
+    this.setPerformedDate(transactionDate);
+    this.processPerformed();
   }
 
 
@@ -192,7 +133,7 @@ public class TransactionRecord extends GenericBean {
    *@param  tmp  The new performedDate value
    */
   public void setPerformedDate(String tmp) {
-    this.performedDate = DateUtils.parseDateString(tmp, "yyyyMMdd");
+    this.performedDate = DateUtils.parseDateString(tmp, "yyyy-MM-dd");
   }
 
 
@@ -228,6 +169,7 @@ public class TransactionRecord extends GenericBean {
    */
   public void setTransactionTime(String transactionTime) {
     this.transactionTime = transactionTime;
+    this.processPerformed();
   }
 
 
@@ -268,6 +210,16 @@ public class TransactionRecord extends GenericBean {
    */
   public void setTaxId(String taxId) {
     this.taxId = taxId;
+  }
+
+
+  /**
+   *  Sets the providerTaxId attribute of the TransactionRecord object
+   *
+   *@param  taxId  The new providerTaxId value
+   */
+  public void setProviderTaxID(String taxId) {
+    this.setTaxId(taxId);
   }
 
 
@@ -322,6 +274,16 @@ public class TransactionRecord extends GenericBean {
 
 
   /**
+   *  Sets the providerNPI attribute of the TransactionRecord object
+   *
+   *@param  npi  The new providerNPI value
+   */
+  public void setProviderNPI(String npi) {
+    this.setNpi(npi);
+  }
+
+
+  /**
    *  Gets the providerId attribute of the TransactionRecord object
    *
    *@return    The providerId value
@@ -362,6 +324,16 @@ public class TransactionRecord extends GenericBean {
 
 
   /**
+   *  Sets the providerLastName attribute of the TransactionRecord object
+   *
+   *@param  nameLast  The new providerLastName value
+   */
+  public void setProviderLastName(String nameLast) {
+    this.setNameLast(nameLast);
+  }
+
+
+  /**
    *  Gets the nameFirst attribute of the TransactionRecord object
    *
    *@return    The nameFirst value
@@ -378,6 +350,16 @@ public class TransactionRecord extends GenericBean {
    */
   public void setNameFirst(String nameFirst) {
     this.nameFirst = nameFirst;
+  }
+
+
+  /**
+   *  Sets the providerFirstName attribute of the TransactionRecord object
+   *
+   *@param  nameFirst  The new providerFirstName value
+   */
+  public void setProviderFirstName(String nameFirst) {
+    this.setNameFirst(nameFirst);
   }
 
 
@@ -417,7 +399,21 @@ public class TransactionRecord extends GenericBean {
    *@param  type  The new type value
    */
   public void setType(String type) {
-    this.type = type;
+    if (type != null) {
+      this.type = type.toUpperCase();
+    } else {
+      this.type = null;
+    }
+  }
+
+
+  /**
+   *  Sets the transactionType attribute of the TransactionRecord object
+   *
+   *@param  type  The new transactionType value
+   */
+  public void setTransactionType(String type) {
+    this.setType(type);
   }
 
 
@@ -477,7 +473,17 @@ public class TransactionRecord extends GenericBean {
    *@param  tmp  The new performed value
    */
   public void setPerformed(String tmp) {
-    this.performed = DateUtils.parseTimestampString(tmp, "yyyyMMddHHmmss");
+    this.performed = DateUtils.parseTimestampString(tmp, "yyyy-MM-dd hh-mm-ss a");
+  }
+
+
+  /**
+   *  Sets the performed attribute of the TransactionRecord object
+   */
+  private void processPerformed() {
+    if (transactionDate != null && transactionTime != null) {
+      this.setPerformed(transactionDate + " " + transactionTime);
+    }
   }
 
 
@@ -539,81 +545,64 @@ public class TransactionRecord extends GenericBean {
    *@exception  SQLException  Description of the Exception
    */
   public boolean insert(Connection db) throws SQLException {
-    //check for is valid?
-    try {
-      db.setAutoCommit(false);
-
-      if (System.getProperty("DEBUG") != null) {
-        System.out.println("TransactionRecord-> Beginning insert");
-      }
-
-      StringBuffer sql = new StringBuffer();
-      sql.append(
-          "INSERT INTO billing_transaction " +
-          "(tax_id, license_no, npi, provider_id, namelast, " +
-          "namefirst, payer_id, type, ");
-      if (performedDate != null) {
-        sql.append("date_performed, ");
-      }
-      if (performed != null) {
-        sql.append("performed, ");
-      }
-      if (entered != null) {
-        sql.append("entered, ");
-      }
-      sql.append("trans_id ) ");
-      sql.append("VALUES (?, ?, ?, ?, ?, ?, ?, ?, ");
-      if (performedDate != null) {
-        sql.append("?, ");
-      }
-      if (performed != null) {
-        sql.append("?, ");
-      }
-      if (entered != null) {
-        sql.append("?, ");
-      }
-      sql.append("?) ");
-
-      int i = 0;
-      PreparedStatement pst = db.prepareStatement(sql.toString());
-      pst.setString(++i, getTaxId());
-      pst.setString(++i, getLicenseNumber());
-      pst.setString(++i, getNpi());
-      pst.setString(++i, getProviderId());
-      pst.setString(++i, getNameLast());
-      pst.setString(++i, getNameFirst());
-      pst.setString(++i, getPayerId());
-      pst.setString(++i, getType());
-      if (performedDate != null) {
-        pst.setDate(++i, performedDate);
-      }
-      if (performed != null) {
-        pst.setTimestamp(++i, performed);
-      }
-      if (entered != null) {
-        pst.setTimestamp(++i, entered);
-      }
-      pst.setString(++i, getTransactionId());
-
-      pst.execute();
-      pst.close();
-
-      if (System.getProperty("DEBUG") != null) {
-        System.out.println("TransactionRecord-> Setting ID");
-      }
-
-      id = DatabaseUtils.getCurrVal(db, "billing_transaction_id_seq");
-      db.commit();
-
-      if (System.getProperty("DEBUG") != null) {
-        System.out.println("TransactionRecord-> TransactionRecord inserted successfully");
-      }
-    } catch (SQLException e) {
-      db.rollback();
-      db.setAutoCommit(true);
-      throw new SQLException(e.getMessage());
+    if (System.getProperty("DEBUG") != null) {
+      System.out.println("TransactionRecord-> Beginning insert");
     }
-    db.setAutoCommit(true);
+    StringBuffer sql = new StringBuffer();
+    sql.append(
+        "INSERT INTO billing_transaction " +
+        "(tax_id, license_no, npi, provider_id, namelast, " +
+        "namefirst, payer_id, type, ");
+    if (performedDate != null) {
+      sql.append("date_performed, ");
+    }
+    if (performed != null) {
+      sql.append("performed, ");
+    }
+    if (entered != null) {
+      sql.append("entered, ");
+    }
+    sql.append("trans_id ) ");
+    sql.append("VALUES (?, ?, ?, ?, ?, ?, ?, ?, ");
+    if (performedDate != null) {
+      sql.append("?, ");
+    }
+    if (performed != null) {
+      sql.append("?, ");
+    }
+    if (entered != null) {
+      sql.append("?, ");
+    }
+    sql.append("?) ");
+    int i = 0;
+    PreparedStatement pst = db.prepareStatement(sql.toString());
+    pst.setString(++i, getTaxId());
+    pst.setString(++i, getLicenseNumber());
+    pst.setString(++i, getNpi());
+    pst.setString(++i, getProviderId());
+    pst.setString(++i, getNameLast());
+    pst.setString(++i, getNameFirst());
+    pst.setString(++i, getPayerId());
+    pst.setString(++i, getType());
+    if (performedDate != null) {
+      pst.setDate(++i, performedDate);
+    }
+    if (performed != null) {
+      pst.setTimestamp(++i, performed);
+    }
+    if (entered != null) {
+      pst.setTimestamp(++i, entered);
+    }
+    pst.setString(++i, getTransactionId());
+    pst.execute();
+    pst.close();
+    if (System.getProperty("DEBUG") != null) {
+      System.out.println("TransactionRecord-> Setting ID");
+    }
+    id = DatabaseUtils.getCurrVal(db, "billing_transaction_id_seq");
+    if (System.getProperty("DEBUG") != null) {
+      System.out.println("TransactionRecord-> TransactionRecord inserted successfully");
+    }
     return true;
   }
 
@@ -641,13 +630,11 @@ public class TransactionRecord extends GenericBean {
    *@param  db                Description of Parameter
    *@return                   Description of the Returned Value
    *@exception  SQLException  Description of Exception
-   *@since                    1.1
    */
   public boolean delete(Connection db) throws SQLException {
     if (id == -1) {
       throw new SQLException("TransactionRecord ID not specified.");
     }
-
     int resultCount = 0;
     PreparedStatement pst = db.prepareStatement(
         "DELETE FROM billing_transaction " +
@@ -655,7 +642,6 @@ public class TransactionRecord extends GenericBean {
     pst.setInt(1, this.getId());
     resultCount = pst.executeUpdate();
     pst.close();
-
     if (resultCount == 0) {
       errors.put("actionError", "Transaction Record (ID: " + getId() + ") could not be deleted.");
       return false;
@@ -704,42 +690,32 @@ public class TransactionRecord extends GenericBean {
    *@param  context           Description of Parameter
    *@return                   The Valid value
    *@exception  SQLException  Description of Exception
-   *@since                    1.12
    */
-
   protected boolean isValid(Connection db, ActionContext context) throws SQLException {
     if (taxId == null || taxId.trim().equals("")) {
       errors.put("taxIdError", "Tax ID cannot be left blank");
     }
-
     if (licenseNumber == null || licenseNumber.trim().equals("")) {
       errors.put("licenseNumberError", "License Number cannot be left blank");
     }
-
     if (nameLast == null || nameLast.trim().equals("")) {
       errors.put("nameLastError", "Last Name cannot be left blank");
     }
-
     if (payerId == null || payerId.trim().equals("")) {
       errors.put("payerIdError", "Payer ID cannot be left blank");
     }
-
     if (type == null || type.trim().equals("")) {
       errors.put("typeError", "Type cannot be left blank");
     }
-
     if (transactionId == null || transactionId.trim().equals("")) {
       errors.put("transactionIdError", "Transaction ID cannot be left blank");
     }
-
     if (transactionDate == null || transactionDate.trim().equals("")) {
       errors.put("transactionDateError", "Transaction Date cannot be left blank");
     }
-
     if (transactionTime == null || transactionTime.trim().equals("")) {
       errors.put("transactionTimeError", "Transaction Time cannot be left blank");
     }
-
     if (hasErrors()) {
       return false;
     } else {
