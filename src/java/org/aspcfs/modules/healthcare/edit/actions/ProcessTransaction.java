3ecfa3a6a2684ba678d92d1bd032dc71f8c75b97
mrajkowski <mrajkowski@concursive.com>
2003-03-24 19:25:20 +0000

    NEW: Added XML processing as the only method for inserting transactions

--- src/java/org/aspcfs/modules/healthcare/edit/actions/ProcessTransaction.java
+++ src/java/org/aspcfs/modules/healthcare/edit/actions/ProcessTransaction.java
@@ -16,9 +16,11 @@ import org.aspcfs.controller.*;
 import org.aspcfs.modules.service.base.*;
 import org.aspcfs.modules.login.base.AuthenticationItem;
 import org.aspcfs.modules.healthcare.edit.base.TransactionRecord;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
 
 /**
- *  Description of the Class
+ *  Processes records that are posted from clients, using XML
  *
  *@author     chris
  *@created    February 11, 2003
@@ -27,7 +29,7 @@ import org.aspcfs.modules.healthcare.edit.base.TransactionRecord;
 public final class ProcessTransaction extends CFSModule {
 
   /**
-   *  Description of the Method
+   *  A single XML transaction is processed
    *
    *@param  context  Description of the Parameter
    *@return          Description of the Return Value
@@ -35,25 +37,24 @@ public final class ProcessTransaction extends CFSModule {
   public String executeCommandDefault(ActionContext context) {
     Exception errorMessage = null;
     Connection db = null;
-    String values = null;
-    TransactionRecord tr = null;
     boolean recordInserted = false;
-    //create a new tramsacton record from the request
-    tr = new TransactionRecord();
-    if (!tr.buildRecord(context.getRequest())) {
-      processErrors(context, tr.getErrors());
-      return ("PacketOK");
-    }
     try {
-      //get a database connection
+      //This is a custom DTD that won't be used by any other processes, so
+      //populate the object here
+      XMLUtils xml = new XMLUtils(context.getRequest());
+      TransactionRecord thisRecord = new TransactionRecord();
+      XMLUtils.populateObject(thisRecord, xml.getDocumentElement());
+      //get a database connection using the vhost context info
       AuthenticationItem auth = new AuthenticationItem();
       db = auth.getConnection(context, false);
-      //try to insert
-      recordInserted = tr.insert(db, context);
-      if (recordInserted) {
-        context.getRequest().setAttribute("Transaction", tr);
-      } else {
-        processErrors(context, tr.getErrors());
+      //Insert the record
+      if (System.getProperty("DEBUG") != null) {
+        System.out.println("ProcessTransaction-> " + thisRecord.getPerformed());
+      }
+      recordInserted = thisRecord.insert(db);
+      //The object might have specified some validation errors
+      if (thisRecord.hasErrors()) {
+        
       }
     } catch (Exception e) {
       errorMessage = e;
@@ -62,6 +63,38 @@ public final class ProcessTransaction extends CFSModule {
       if (db != null) {
         this.freeConnection(context, db);
       }
+    }
+    //Prepare the transaction result/response
+    TransactionStatus thisMessage = new TransactionStatus();
+    if (errorMessage == null) {
+      thisMessage.setStatusCode(0);
+    } else {
+      thisMessage.setStatusCode(1);
+      thisMessage.setMessage(errorMessage.getMessage());
+    }
+    //Construct the XML response
+    try {
+      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = dbf.newDocumentBuilder();
+      Document document = builder.newDocument();
+      Element app = document.createElement("aspcfs");
+      document.appendChild(app);
+      //Append the response
+      Element response = document.createElement("response");
+      app.appendChild(response);
+      //Append the status code (0=OK, 1=Error)
+      Element status = document.createElement("status");
+      status.appendChild(document.createTextNode(String.valueOf(thisMessage.getStatusCode())));
+      response.appendChild(status);
+      //Append the errorText
+      Element errorText = document.createElement("errorText");
+      if (thisMessage.getStatusCode() > 0) {
+        errorText.appendChild(document.createTextNode(thisMessage.getMessage()));
+      }
+      response.appendChild(errorText);
+      context.getRequest().setAttribute("statusXML", XMLUtils.toString(document));
+    } catch (Exception pce) {
+      pce.printStackTrace(System.out);
     }
     return ("PacketOK");
   }
