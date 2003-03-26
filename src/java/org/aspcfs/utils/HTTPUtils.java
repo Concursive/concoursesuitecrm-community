fc8a957b36e28525e9ac5ac50aced10ff4981f9b
mrajkowski <mrajkowski@concursive.com>
2003-03-26 21:14:34 +0000

    NEW: Added SSL posting to self-signed web sites, bypasses ALL security so use sparingly

--- src/java/org/aspcfs/utils/HTTPUtils.java
+++ src/java/org/aspcfs/utils/HTTPUtils.java
@@ -2,6 +2,10 @@ package org.aspcfs.utils;
 
 import java.io.*;
 import java.net.*;
+import javax.net.*;
+import javax.net.ssl.*;
+import java.security.cert.CertificateFactory;
+import javax.security.cert.*;
 
 /**
  *  Utilities for working with HTTP
@@ -38,7 +42,9 @@ public class HTTPUtils {
 
 
   /**
-   *  Sends a string to the specified URL
+   *  Sends a string to the specified URL, intended for communicating
+   *  with web servers.  Use the SSLMessage for secure communication with
+   *  a server application.
    *
    *@param  address                  Description of the Parameter
    *@param  xmlPacket                Description of the Parameter
@@ -49,17 +55,43 @@ public class HTTPUtils {
     Exception errorMessage = null;
     try {
       URL url = new URL(address);
-      HttpURLConnection http = (HttpURLConnection) url.openConnection();
-      http.setRequestMethod("POST");
-      http.setRequestProperty("Content-Type", "text/xml; charset=\"utf-8\"");
-      http.setDoOutput(true);
-      PrintWriter outStream = new PrintWriter(http.getOutputStream());
-      outStream.println(xmlPacket);
-      outStream.close();
-      return (retrieveHtml(http));
+      if (address.indexOf("https://") > -1) {
+        //The default factory requires a trusted certificate
+        //SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        //Accept any certificate using the custom TrustManager
+        X509TrustManager xtm = new HttpsTrustManager();
+        TrustManager mytm[] = {xtm};
+        SSLContext ctx = SSLContext.getInstance("SSL");
+        ctx.init(null, mytm, null);
+        SSLSocketFactory factory = ctx.getSocketFactory();
+        //Post the data
+        HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+        https.setSSLSocketFactory(factory);
+        https.setRequestMethod("POST");
+        https.setRequestProperty("Content-Type", "text/xml; charset=\"utf-8\"");
+        https.setDoInput(true);
+        https.setDoOutput(true);
+        PrintWriter outStream = new PrintWriter(https.getOutputStream());
+        outStream.println(xmlPacket);
+        outStream.close();
+        return (retrieveHtml(https));
+      } else {
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("POST");
+        http.setRequestProperty("Content-Type", "text/xml; charset=\"utf-8\"");
+        http.setDoOutput(true);
+        PrintWriter outStream = new PrintWriter(http.getOutputStream());
+        outStream.println(xmlPacket);
+        outStream.close();
+        return (retrieveHtml(http));
+      }
     } catch (java.net.MalformedURLException e) {
       errorMessage = e;
     } catch (java.io.IOException e) {
+      errorMessage = e;
+    } catch (java.security.KeyManagementException e) {
+      errorMessage = e;
+    } catch (java.security.NoSuchAlgorithmException e) {
       errorMessage = e;
     }
     if (errorMessage != null) {
