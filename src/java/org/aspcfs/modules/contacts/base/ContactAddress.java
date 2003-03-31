aecc08a5b326c37ca9dc7db95f4958c8885a70b2
akhi_m <akhi_m@concursive.com>
2003-03-31 15:18:16 +0000

    New : MERGE from f-contact_addrline3_state

--- src/java/org/aspcfs/modules/contacts/base/ContactAddress.java
+++ src/java/org/aspcfs/modules/contacts/base/ContactAddress.java
@@ -76,7 +76,9 @@ public class ContactAddress extends Address {
     Statement st = null;
     ResultSet rs = null;
     StringBuffer sql = new StringBuffer();
-    sql.append("SELECT * " +
+    sql.append("SELECT c.address_id, c.contact_id, c.address_type, c.addrline1, c.addrline1,  " +
+        "c.addrline2, c.addrline3, c.city, c.state, c.country, c.postalcode, c.entered, c.enteredby, " +
+        "c.modified, c.modifiedby, l.description " +
         "FROM contact_address c, lookup_contactaddress_types l " +
         "WHERE c.address_type = l.code " +
         "AND address_id = " + addressId + " ");
@@ -104,10 +106,10 @@ public class ContactAddress extends Address {
    */
   public void process(Connection db, int contactId, int enteredBy, int modifiedBy) throws SQLException {
     if (this.getEnabled() == true) {
-      if("-1".equals(this.getState())){
+      if ("-1".equals(this.getState())) {
         this.setState("");
       }
-      if("-1".equals(this.getCountry())){
+      if ("-1".equals(this.getCountry())) {
         this.setCountry("");
       }
       if (this.getId() == -1) {
@@ -147,7 +149,7 @@ public class ContactAddress extends Address {
   public void insert(Connection db, int contactId, int enteredBy) throws SQLException {
     StringBuffer sql = new StringBuffer();
     sql.append("INSERT INTO contact_address " +
-        "(contact_id, address_type, addrline1, addrline2, city, state, postalcode, country, ");
+        "(contact_id, address_type, addrline1, addrline2, addrline3, city, state, postalcode, country, ");
     if (this.getEntered() != null) {
       sql.append("entered, ");
     }
@@ -155,7 +157,7 @@ public class ContactAddress extends Address {
       sql.append("modified, ");
     }
     sql.append("enteredBy, modifiedBy ) ");
-    sql.append("VALUES (?, ?, ?, ?, ?, ?, ?, ?, ");
+    sql.append("VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ");
     if (this.getEntered() != null) {
       sql.append("?, ");
     }
@@ -179,8 +181,13 @@ public class ContactAddress extends Address {
     }
     pst.setString(++i, this.getStreetAddressLine1());
     pst.setString(++i, this.getStreetAddressLine2());
+    pst.setString(++i, this.getStreetAddressLine3());
     pst.setString(++i, this.getCity());
-    pst.setString(++i, this.getState());
+    if ("UNITED STATES".equals(this.getCountry()) || "CANADA".equals(this.getCountry())) {
+      pst.setString(++i, this.getState());
+    } else {
+      pst.setString(++i, this.getOtherState());
+    }
     pst.setString(++i, this.getZip());
     pst.setString(++i, this.getCountry());
 
@@ -193,7 +200,6 @@ public class ContactAddress extends Address {
 
     pst.setInt(++i, this.getEnteredBy());
     pst.setInt(++i, this.getModifiedBy());
-
     pst.execute();
     pst.close();
 
@@ -212,7 +218,7 @@ public class ContactAddress extends Address {
   public void update(Connection db, int modifiedBy) throws SQLException {
     PreparedStatement pst = db.prepareStatement(
         "UPDATE contact_address " +
-        "SET address_type = ?, addrline1 = ?, addrline2 = ?, city = ?, state = ?, postalcode = ?, country = ?, " +
+        "SET address_type = ?, addrline1 = ?, addrline2 = ?, addrline3 = ?, city = ?, state = ?, postalcode = ?, country = ?, " +
         "modifiedby = ?, modified = CURRENT_TIMESTAMP " +
         "WHERE address_id = ? ");
     int i = 0;
@@ -223,8 +229,13 @@ public class ContactAddress extends Address {
     }
     pst.setString(++i, this.getStreetAddressLine1());
     pst.setString(++i, this.getStreetAddressLine2());
+    pst.setString(++i, this.getStreetAddressLine3());
     pst.setString(++i, this.getCity());
-    pst.setString(++i, this.getState());
+    if ("UNITED STATES".equals(this.getCountry()) || "CANADA".equals(this.getCountry())) {
+      pst.setString(++i, this.getState());
+    } else {
+      pst.setString(++i, this.getOtherState());
+    }
     pst.setString(++i, this.getZip());
     pst.setString(++i, this.getCountry());
     pst.setInt(++i, modifiedBy);
