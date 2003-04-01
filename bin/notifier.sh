9bb27b49d0caf6f92f7df3028ccf6e1ae41561cd
mrajkowski <mrajkowski@concursive.com>
2003-04-01 14:59:26 +0000

    REMOVED: no longer needed, replaced with jcrontab

--- bin/notifier.sh
+++
@@ -1,5 +0,0 @@
-# This is a temporary script until jcrontab is implemented to run the notifier
-# Must run "ant deploy" first
-# $Id$
-source /etc/profile
-ant -f ${CFS_HOME}/WEB-INF/build.xml run.notifier
