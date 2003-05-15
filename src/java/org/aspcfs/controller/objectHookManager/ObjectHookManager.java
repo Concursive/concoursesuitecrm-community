b5b70b6dc06f1672cef46514bb04f4f03485dbf3
mrajkowski <mrajkowski@concursive.com>
2003-05-15 15:39:41 +0000

    NEW: Added a method to execute a business process, can be triggered by a cron event or executed manually

--- src/java/org/aspcfs/controller/objectHookManager/ObjectHookManager.java
+++ src/java/org/aspcfs/controller/objectHookManager/ObjectHookManager.java
@@ -11,13 +11,16 @@ import java.io.*;
 import com.darkhorseventures.framework.actions.*;
 import com.darkhorseventures.database.*;
 import org.aspcfs.modules.service.base.PacketContext;
+import javax.servlet.ServletContext;
+
 /**
  *  Manages hooks within the application and attaches to a WorkflowManager to
  *  execute BusinessProcess objects as defined by ObjectHook objects.
  *
  *@author     matt rajkowski
  *@created    November 11, 2002
- *@version    $Id$
+ *@version    $Id: ObjectHookManager.java,v 1.9 2003/05/08 13:50:18 mrajkowski
+ *      Exp $
  */
 public class ObjectHookManager {
 
@@ -41,9 +44,16 @@ public class ObjectHookManager {
     this.fileLibraryPath = tmp;
   }
 
+
+  /**
+   *  Gets the processList attribute of the ObjectHookManager object
+   *
+   *@return    The processList value
+   */
   public BusinessProcessList getProcessList() {
     return processList;
   }
+
 
   /**
    *  Description of the Method
@@ -103,7 +113,10 @@ public class ObjectHookManager {
 
 
   /**
-   *  Description of the Method
+   *  Executes a business process based on the objects that are specified. The
+   *  objectHook must already be loaded into the hookList, with a corresponding
+   *  action to trigger on: insert, update, or delete. Also, the business
+   *  process must already be loaded into the business process list.
    *
    *@param  actionContext   Description of the Parameter
    *@param  action          Description of the Parameter
@@ -129,6 +142,7 @@ public class ObjectHookManager {
           if (System.getProperty("DEBUG") != null) {
             System.out.println("ObjectHookList-> Hook thread start");
           }
+          //Thread the business process
           ObjectHook thisHook = new ObjectHook(context);
           thisHook.setActionId(action);
           thisHook.setObjectHookList(hookList);
@@ -137,12 +151,58 @@ public class ObjectHookManager {
           thisHook.start();
         }
       } else {
-        if (System.getProperty("DEBUG") != null) {
+        if (wfManager == null) {
           System.out.println("ObjectHookManager-> WorkflowManager not found");
+        }
+        if (processList == null) {
+          System.out.println("ObjectHookManager-> ProcessList not found");
         }
       }
     } catch (ClassNotFoundException e) {
       e.printStackTrace(System.out);
+    }
+  }
+
+
+  /**
+   *  Executes a specified business process. The business process must already
+   *  be loaded into the business process list.<p>
+   *
+   *  Commonly used for business processes that are triggered by a cron, or
+   *  manually triggered.
+   *
+   *@param  servletContext  Description of the Parameter
+   *@param  processName     Description of the Parameter
+   *@param  sqlDriver       Description of the Parameter
+   *@param  ce              Description of the Parameter
+   */
+  public void process(ServletContext servletContext, String processName, ConnectionPool sqlDriver, ConnectionElement ce) {
+    WorkflowManager wfManager = (WorkflowManager) servletContext.getAttribute("WorkflowManager");
+    if (wfManager != null && processList != null) {
+      ComponentContext context = new ComponentContext();
+      context.setParameter("FileLibraryPath", fileLibraryPath);
+      context.setAttribute("ConnectionPool", sqlDriver);
+      context.setAttribute("ConnectionElement", ce);
+      context.setAttribute("ClientSSLKeystore", servletContext.getAttribute("ClientSSLKeystore"));
+      context.setAttribute("ClientSSLKeystorePassword", servletContext.getAttribute("ClientSSLKeystorePassword"));
+      if (System.getProperty("DEBUG") != null) {
+        System.out.println("ObjectHookList-> Hook thread start for process: " + processName);
+      }
+      //Thread the business process
+      ObjectHook thisHook = new ObjectHook(context);
+      thisHook.setBusinessProcess(processName);
+      thisHook.setBusinessProcessList(processList);
+      thisHook.setManager(wfManager);
+      thisHook.start();
+    } else {
+      if (System.getProperty("DEBUG") != null) {
+        if (wfManager == null) {
+          System.out.println("ObjectHookManager-> WorkflowManager not found");
+        }
+        if (processList == null) {
+          System.out.println("ObjectHookManager-> ProcessList not found");
+        }
+      }
     }
   }
 }
