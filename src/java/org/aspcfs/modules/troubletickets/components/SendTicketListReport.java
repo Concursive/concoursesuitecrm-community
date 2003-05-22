da09a3b33d3b44824e50eee014070d65d800e117
mrajkowski <mrajkowski@concursive.com>
2003-05-22 19:29:44 +0000

    CHANGE: working version which takes a ticket list and emails it out based on parameters

--- src/java/org/aspcfs/modules/troubletickets/components/SendTicketListReport.java
+++ src/java/org/aspcfs/modules/troubletickets/components/SendTicketListReport.java
@@ -5,24 +5,42 @@ package org.aspcfs.modules.troubletickets.components;
 import org.aspcfs.controller.*;
 import org.aspcfs.apps.workFlowManager.*;
 import org.aspcfs.controller.objectHookManager.*;
-import org.aspcfs.utils.StringUtils;
-import org.aspcfs.modules.base.Notification;
 import org.aspcfs.modules.troubletickets.base.*;
-import java.sql.*;
 import org.aspcfs.modules.components.SendUserNotification;
+import org.aspcfs.modules.components.EmailDigestUtil;
 import java.util.*;
+import java.sql.Connection;
 
+/**
+ *  Using a supplied ticket list, SendTicketListReport will compile the tickets
+ *  and send notifications based on the process parameters.
+ *
+ *@author     matt rajkowski
+ *@created    May 22, 2003
+ *@version    $Id$
+ */
 public class SendTicketListReport extends ObjectHookComponent implements ComponentInterface {
+
   public final static String REPORT_TICKET_CONTENT = "report.ticket.content";
-  public final static String EMAIL_TO = "notification.to";
-  public final static String USER_TO = "notification.users.to";
-  public final static String CONTACT_TO = "notification.contacts.to";
-  public final static String DEPARTMENT_TO = "notification.departments.to";
-  
+  private final static String PREFIX = "TICKET";
+
+
+  /**
+   *  Gets the description attribute of the SendTicketListReport object
+   *
+   *@return    The description value
+   */
   public String getDescription() {
     return "Sends a ticket report to specified users with the specified parameters";
   }
 
+
+  /**
+   *  Description of the Method
+   *
+   *@param  context  Description of the Parameter
+   *@return          Description of the Return Value
+   */
   public boolean execute(ComponentContext context) {
     if (!context.hasObjects()) {
       return false;
@@ -31,76 +49,28 @@ public class SendTicketListReport extends ObjectHookComponent implements Compone
     Connection db = null;
     HashMap mailList = new HashMap();
     TicketList tickets = (TicketList) context.getObjects();
-    Iterator i = tickets.iterator();
     try {
       db = this.getConnection(context);
-      String emailTo = context.getParameter(EMAIL_TO);
-      mailList.put(emailTo, new ArrayList());
-      //Build message HashMap so that recipient will get a single email
+      //Prepare the LoadTicketDetails component to load each ticket into the context
+      LoadTicketDetails loadTicket = new LoadTicketDetails();
+      Iterator i = tickets.iterator();
       while (i.hasNext()) {
         Ticket thisTicket = (Ticket) i.next();
-        System.out.println("Adding ticket: " + thisTicket.getId());
-        //Determine who notification is for: variable user ids, contact ids, department ids
-        String userTo = context.getParameter(USER_TO, thisTicket, null);
-        String contactTo = context.getParameter(CONTACT_TO, thisTicket, null);
-        String departmentTo = context.getParameter(DEPARTMENT_TO, thisTicket, null);
+        context.setThisObject(thisTicket);
+        loadTicket.execute(context);
         //Determine the content of the message to send
         String thisMessage = context.getParameter(REPORT_TICKET_CONTENT, thisTicket, null);
-        System.out.println(thisMessage);
-        //Get a reference to the person, creating one if new
-        ArrayList emailToList = (ArrayList) mailList.get(emailTo);
-        emailToList.add(thisMessage);
-        
-        //Users: Lookup each user id
-        
-        //Contacts: Lookup each contact id
-        
+        //emailTo gets all the messages
+        EmailDigestUtil.appendEmailAddresses(mailList, context.getParameter(SendUserNotification.EMAIL_TO), thisMessage, PREFIX + thisTicket.getId());
+        //Users: Lookup each user id specified
+        EmailDigestUtil.appendEmailUsers(db, mailList, context.getParameter(SendUserNotification.USER_TO, thisTicket, null), thisMessage, PREFIX + thisTicket.getId());
+        //Contacts: Lookup each contact id specified
+        EmailDigestUtil.appendEmailContacts(db, mailList, context.getParameter(SendUserNotification.CONTACT_TO, thisTicket, null), thisMessage, PREFIX + thisTicket.getId());
         //Departments: Get list of enabled users in the specified department, lookup each user id
-        
-        //Emails
-        
-        
-        
-        
+        //EmailDigestUtil.appendDepartmentContacts(db, mailList, context.getParameter(SendUserNotification.DEPARTMENT_TO, thisTicket, null), thisMessage, PREFIX + thisTicket.getId());
       }
-      
-      //Process the HashMap and send emails
-      Iterator toList = mailList.keySet().iterator();
-      while (toList.hasNext()) {
-        //Either
-        //Set context parameters and execute component from cache
-        //context.execute("org.aspcfs.modules.components.SendUserNotification");
-        
-        //Or
-        //Create a new notification directly, configure, and execute
-        StringBuffer messageDigest = new StringBuffer();
-        String emailAddressTo = (String) toList.next();
-        ArrayList emailToList = (ArrayList) mailList.get(emailAddressTo);
-        Iterator messages = emailToList.iterator();
-        while (messages.hasNext()) {
-          messageDigest.append((String) messages.next());
-        }
-        Notification thisNotification = new Notification();
-        thisNotification.setSubject(StringUtils.toHtml(context.getParameter(SendUserNotification.SUBJECT)));
-        thisNotification.setFrom(StringUtils.toHtml(context.getParameter(SendUserNotification.FROM)));
-        thisNotification.setType(Notification.EMAIL);
-        thisNotification.setEmailToNotify(emailAddressTo);
-        thisNotification.setMessageToSend(StringUtils.toHtml(context.getParameter(SendUserNotification.BODY) + messageDigest.toString()));
-        String host = context.getParameter(SendUserNotification.HOST);
-        if (host != null && !"".equals(host)) {
-          thisNotification.setHost(host);
-        }
-        thisNotification.notifyAddress();
-/*
-        if int {
-          thisNotification.setUserToNotify();
-          thisNotification.notifyUser(db);
-        } else {
-          thisNotification.setTo();
-          thisNotification.notify(db);
-        }
-*/
-      }
+      //Send the compiled messages
+      EmailDigestUtil.sendMail(context, mailList);
       result = true;
     } catch (Exception e) {
       e.printStackTrace(System.out);
@@ -109,5 +79,6 @@ public class SendTicketListReport extends ObjectHookComponent implements Compone
     }
     return result;
   }
+
 }
 
