65b5464fc4dccb2c3d056232de5fec9d4a53efcd
akhi_m <akhi_m@concursive.com>
2003-06-10 18:52:12 +0000

    NEW: Ticket Category draft table added.

--- src/sql/postgresql/new_tms.sql
+++ src/sql/postgresql/new_tms.sql
@@ -55,6 +55,17 @@ CREATE TABLE ticket_category (
   ,enabled BOOLEAN DEFAULT true
 );
 
+CREATE TABLE ticket_category_draft (
+  id serial PRIMARY KEY,
+  link_id INT DEFAULT -1,
+  cat_level int  NOT NULL DEFAULT 0,
+  parent_cat_code int  NOT NULL,
+  description VARCHAR(300) NOT NULL,
+  full_description text NOT NULL DEFAULT '',
+  default_item BOOLEAN DEFAULT false,
+  level INTEGER DEFAULT 0,
+  enabled BOOLEAN DEFAULT true
+);
 
 CREATE TABLE ticket (
   ticketid SERIAL PRIMARY KEY,
