4ef5b104039ebacdbeb0c2994a1dd46e663be276
akhi_m <akhi_m@concursive.com>
2003-07-11 16:58:32 +0000

    FIX: Removed the display properties NEW: Added tips, notes, features and business rule lists and building them

--- src/java/org/aspcfs/modules/help/base/HelpItem.java
+++ src/java/org/aspcfs/modules/help/base/HelpItem.java
@@ -7,9 +7,8 @@ import java.sql.*;
 import org.aspcfs.utils.DatabaseUtils;
 import java.io.*;
 
-
 /**
- *  Description of the Class
+ *  Represents help for a page in CFS
  *
  *@author     matt rajkowski
  *@created    January 21, 2002
@@ -22,16 +21,22 @@ public class HelpItem extends GenericBean {
   private String module = null;
   private String section = null;
   private String subsection = null;
-  private String display1 = null;
-  private String display2 = null;
-  private String display3 = null;
-  private String display4 = null;
   private String description = null;
   private String permission = null;
   private int enteredBy = -1;
   private java.sql.Timestamp entered = null;
   private int modifiedBy = -1;
   private java.sql.Timestamp modified = null;
+  private boolean buildFeatures = false;
+  private boolean buildRules = false;
+  private boolean buildNotes = false;
+  private boolean buildTips = false;
+
+  //details
+  private HelpFeatureList features = new HelpFeatureList();
+  private HelpNoteList notes = new HelpNoteList();
+  private HelpTipList tips = new HelpTipList();
+  private HelpBusinessRuleList businessRules = new HelpBusinessRuleList();
 
 
   /**
@@ -58,9 +63,10 @@ public class HelpItem extends GenericBean {
    *@param  module            Description of the Parameter
    *@param  section           Description of the Parameter
    *@param  subsection        Description of the Parameter
+   *@param  userId            Description of the Parameter
    *@exception  SQLException  Description of the Exception
    */
-  public HelpItem(Connection db, String module, String section, String subsection) throws SQLException {
+  public HelpItem(Connection db, String module, String section, String subsection, int userId) throws SQLException {
     if ("null".equals(module)) {
       module = null;
     }
@@ -70,12 +76,72 @@ public class HelpItem extends GenericBean {
     if ("null".equals(subsection)) {
       subsection = null;
     }
+    processRecord(db, userId);
+  }
+
+
+  /**
+   *Constructor for the HelpItem object
+   *
+   *@param  db                Description of the Parameter
+   *@param  id                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public HelpItem(Connection db, int id) throws SQLException {
+    this.id = id;
+    queryRecord(db);
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void queryRecord(Connection db) throws SQLException {
     PreparedStatement pst = null;
     ResultSet rs = null;
     String sql =
         "SELECT * " +
         "FROM help_contents h " +
-        "WHERE module = ? AND section = ? AND subsection = ? ";
+        "WHERE help_id = ? ";
+    pst = db.prepareStatement(sql);
+    if (System.getProperty("DEBUG") != null) {
+      System.out.println("HelpItem-> Prepared");
+    }
+    int i = 0;
+    pst.setInt(++i, id);
+    rs = pst.executeQuery();
+    if (rs.next()) {
+      buildRecord(rs);
+    }
+    rs.close();
+    pst.close();
+
+    buildFeatures(db);
+    buildRules(db);
+    buildNotes(db);
+    buildTips(db);
+  }
+
+
+  /**
+   *  Description of the Method
+   *
+   *@param  db                Description of the Parameter
+   *@param  userId            Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public synchronized void processRecord(Connection db, int userId) throws SQLException {
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+    boolean newItem = true;
+    String sql =
+        "SELECT * " +
+        "FROM help_contents h " +
+        "WHERE module = ? AND section = ? " +
+        (subsection != null ? "AND subsection = ? " : "");
     pst = db.prepareStatement(sql);
     if (System.getProperty("DEBUG") != null) {
       System.out.println("HelpItem-> Prepared");
@@ -83,19 +149,45 @@ public class HelpItem extends GenericBean {
     int i = 0;
     pst.setString(++i, module);
     pst.setString(++i, section);
-    pst.setString(++i, subsection);
+    if (subsection != null) {
+      pst.setString(++i, subsection);
+    }
     rs = pst.executeQuery();
     if (rs.next()) {
       buildRecord(rs);
-    } else {
-      this.module = module;
-      this.section = section;
-      this.subsection = subsection;
+      newItem = false;
     }
     rs.close();
     pst.close();
     if (System.getProperty("DEBUG") != null) {
       System.out.println("HelpItem-> Init ok");
+    }
+
+    //check for a new entry
+    if (newItem) {
+      this.setEnteredBy(userId);
+      this.setModifiedBy(userId);
+      this.insert(db);
+      if (System.getProperty("DEBUG") != null) {
+        System.out.println("HelpItem-> Record not present.. Inserting new record ");
+      }
+    } else {
+      //build general features
+      if (buildFeatures) {
+        buildFeatures(db);
+      }
+      //build business rules
+      if (buildRules) {
+        buildRules(db);
+      }
+      //build notes
+      if (buildNotes) {
+        buildNotes(db);
+      }
+      //build tips
+      if (buildTips) {
+        buildTips(db);
+      }
     }
   }
 
@@ -147,46 +239,6 @@ public class HelpItem extends GenericBean {
    */
   public void setSubsection(String tmp) {
     this.subsection = tmp;
-  }
-
-
-  /**
-   *  Sets the display1 attribute of the HelpItem object
-   *
-   *@param  tmp  The new display1 value
-   */
-  public void setDisplay1(String tmp) {
-    this.display1 = tmp;
-  }
-
-
-  /**
-   *  Sets the display2 attribute of the HelpItem object
-   *
-   *@param  tmp  The new display2 value
-   */
-  public void setDisplay2(String tmp) {
-    this.display2 = tmp;
-  }
-
-
-  /**
-   *  Sets the display3 attribute of the HelpItem object
-   *
-   *@param  tmp  The new display3 value
-   */
-  public void setDisplay3(String tmp) {
-    this.display3 = tmp;
-  }
-
-
-  /**
-   *  Sets the display4 attribute of the HelpItem object
-   *
-   *@param  tmp  The new display4 value
-   */
-  public void setDisplay4(String tmp) {
-    this.display4 = tmp;
   }
 
 
@@ -251,6 +303,166 @@ public class HelpItem extends GenericBean {
 
 
   /**
+   *  Sets the features attribute of the HelpItem object
+   *
+   *@param  features  The new features value
+   */
+  public void setFeatures(HelpFeatureList features) {
+    this.features = features;
+  }
+
+
+  /**
+   *  Sets the buildFeatures attribute of the HelpItem object
+   *
+   *@param  buildFeatures  The new buildFeatures value
+   */
+  public void setBuildFeatures(boolean buildFeatures) {
+    this.buildFeatures = buildFeatures;
+  }
+
+
+  /**
+   *  Sets the businessRules attribute of the HelpItem object
+   *
+   *@param  businessRules  The new businessRules value
+   */
+  public void setBusinessRules(HelpBusinessRuleList businessRules) {
+    this.businessRules = businessRules;
+  }
+
+
+  /**
+   *  Sets the buildRules attribute of the HelpItem object
+   *
+   *@param  buildRules  The new buildRules value
+   */
+  public void setBuildRules(boolean buildRules) {
+    this.buildRules = buildRules;
+  }
+
+
+  /**
+   *  Sets the buildNotes attribute of the HelpItem object
+   *
+   *@param  buildNotes  The new buildNotes value
+   */
+  public void setBuildNotes(boolean buildNotes) {
+    this.buildNotes = buildNotes;
+  }
+
+
+  /**
+   *  Sets the buildTips attribute of the HelpItem object
+   *
+   *@param  buildTips  The new buildTips value
+   */
+  public void setBuildTips(boolean buildTips) {
+    this.buildTips = buildTips;
+  }
+
+
+  /**
+   *  Sets the notes attribute of the HelpItem object
+   *
+   *@param  notes  The new notes value
+   */
+  public void setNotes(HelpNoteList notes) {
+    this.notes = notes;
+  }
+
+
+  /**
+   *  Sets the tips attribute of the HelpItem object
+   *
+   *@param  tips  The new tips value
+   */
+  public void setTips(HelpTipList tips) {
+    this.tips = tips;
+  }
+
+
+  /**
+   *  Gets the notes attribute of the HelpItem object
+   *
+   *@return    The notes value
+   */
+  public HelpNoteList getNotes() {
+    return notes;
+  }
+
+
+  /**
+   *  Gets the tips attribute of the HelpItem object
+   *
+   *@return    The tips value
+   */
+  public HelpTipList getTips() {
+    return tips;
+  }
+
+
+  /**
+   *  Gets the buildRules attribute of the HelpItem object
+   *
+   *@return    The buildRules value
+   */
+  public boolean getBuildRules() {
+    return buildRules;
+  }
+
+
+  /**
+   *  Gets the buildNotes attribute of the HelpItem object
+   *
+   *@return    The buildNotes value
+   */
+  public boolean getBuildNotes() {
+    return buildNotes;
+  }
+
+
+  /**
+   *  Gets the buildTips attribute of the HelpItem object
+   *
+   *@return    The buildTips value
+   */
+  public boolean getBuildTips() {
+    return buildTips;
+  }
+
+
+  /**
+   *  Gets the businessRules attribute of the HelpItem object
+   *
+   *@return    The businessRules value
+   */
+  public HelpBusinessRuleList getBusinessRules() {
+    return businessRules;
+  }
+
+
+  /**
+   *  Gets the buildFeatures attribute of the HelpItem object
+   *
+   *@return    The buildFeatures value
+   */
+  public boolean getBuildFeatures() {
+    return buildFeatures;
+  }
+
+
+  /**
+   *  Gets the features attribute of the HelpItem object
+   *
+   *@return    The features value
+   */
+  public HelpFeatureList getFeatures() {
+    return features;
+  }
+
+
+  /**
    *  Gets the id attribute of the HelpItem object
    *
    *@return    The id value
@@ -287,46 +499,6 @@ public class HelpItem extends GenericBean {
    */
   public String getSubsection() {
     return subsection;
-  }
-
-
-  /**
-   *  Gets the display1 attribute of the HelpItem object
-   *
-   *@return    The display1 value
-   */
-  public String getDisplay1() {
-    return display1;
-  }
-
-
-  /**
-   *  Gets the display2 attribute of the HelpItem object
-   *
-   *@return    The display2 value
-   */
-  public String getDisplay2() {
-    return display2;
-  }
-
-
-  /**
-   *  Gets the display3 attribute of the HelpItem object
-   *
-   *@return    The display3 value
-   */
-  public String getDisplay3() {
-    return display3;
-  }
-
-
-  /**
-   *  Gets the display4 attribute of the HelpItem object
-   *
-   *@return    The display4 value
-   */
-  public String getDisplay4() {
-    return display4;
   }
 
 
@@ -401,10 +573,6 @@ public class HelpItem extends GenericBean {
     module = rs.getString("module");
     section = rs.getString("section");
     subsection = rs.getString("subsection");
-    display1 = rs.getString("display1");
-    display2 = rs.getString("display2");
-    display3 = rs.getString("display3");
-    display4 = rs.getString("display4");
     description = rs.getString("description");
     enteredBy = rs.getInt("enteredby");
     entered = rs.getTimestamp("entered");
@@ -427,17 +595,13 @@ public class HelpItem extends GenericBean {
     PreparedStatement pst = null;
     String sql =
         "INSERT INTO help_contents " +
-        "(module, section, subsection, display1, display2, display3, display4, description, enteredby, modifiedby) " +
-        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
+        "(module, section, subsection, description, enteredby, modifiedby) " +
+        "VALUES (?, ?, ?, ?, ?, ?) ";
     pst = db.prepareStatement(sql);
     int i = 0;
     pst.setString(++i, module);
     pst.setString(++i, section);
     pst.setString(++i, subsection);
-    pst.setString(++i, display1);
-    pst.setString(++i, display2);
-    pst.setString(++i, display3);
-    pst.setString(++i, display4);
     pst.setString(++i, description);
     pst.setInt(++i, enteredBy);
     pst.setInt(++i, modifiedBy);
@@ -487,6 +651,54 @@ public class HelpItem extends GenericBean {
     pst.close();
 
     return count;
+  }
+
+
+  /**
+   *  Builds features of this help item
+   *
+   *@param  db                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void buildFeatures(Connection db) throws SQLException {
+    features.setLinkHelpId(this.getId());
+    features.buildList(db);
+  }
+
+
+  /**
+   *  Builds Notes for this help item
+   *
+   *@param  db                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void buildNotes(Connection db) throws SQLException {
+    notes.setLinkHelpId(this.getId());
+    notes.buildList(db);
+  }
+
+
+  /**
+   *  Builds Tips for this help item
+   *
+   *@param  db                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void buildTips(Connection db) throws SQLException {
+    tips.setLinkHelpId(this.getId());
+    tips.buildList(db);
+  }
+
+
+  /**
+   *  Builds business rules for this help item
+   *
+   *@param  db                Description of the Parameter
+   *@exception  SQLException  Description of the Exception
+   */
+  public void buildRules(Connection db) throws SQLException {
+    businessRules.setLinkHelpId(this.getId());
+    businessRules.buildList(db);
   }
 
 
