4f1ef7b0803e85e9406abeaaea9a5975f6961a93
mrajkowski <mrajkowski@concursive.com>
2003-07-31 15:31:49 +0000

    NEW: added URL to JPEG methods (platform dependent)

--- src/java/org/aspcfs/utils/ImageUtils.java
+++ src/java/org/aspcfs/utils/ImageUtils.java
@@ -95,5 +95,105 @@ public class ImageUtils {
       return (1);
     }
   }
+
+
+  /**
+   *  Uses HtmlDoc command line tool, pipes an HTML URL to GhostScript
+   *  which outputs a JPEG file with the given dimensions.
+   *
+   *@param  url        Description of the Parameter
+   *@param  filename   Description of the Parameter
+   *@param  maxWidth   Description of the Parameter
+   *@param  maxHeight  Description of the Parameter
+   *@return            Description of the Return Value
+   */
+  public static int urlToJpegThumbnail(String url, String filename, double maxWidth, double maxHeight) {
+    Process process;
+    Runtime runtime;
+    java.io.InputStream input;
+    byte buffer[];
+    int bytes;
+    String command[] = {"/bin/sh", "-c", "/usr/bin/htmldoc --quiet --jpeg " +
+        "--webpage -t ps --left 0 --top 0 " +
+        "--header ... --footer ... --landscape " + url + " " +
+        "| /usr/bin/gs -q -sDEVICE=jpeg -dNOPAUSE -dBATCH -sOutputFile=- -"};
+    runtime = Runtime.getRuntime();
+    try {
+      process = runtime.exec(command);
+      input = process.getInputStream();
+      BufferedImage originalImage = ImageIO.read(input);
+
+      //Calculate scaling
+      double ratioWidth = maxWidth / originalImage.getWidth();
+      double ratioHeight = maxHeight / originalImage.getHeight();
+      double ratio = 1;
+      if (maxWidth > 0 && maxHeight < 0) {
+        ratio = ratioWidth;
+      } else if (maxHeight > 0 && maxWidth < 0) {
+        ratio = ratioHeight;
+      } else {
+        if (ratioWidth < ratioHeight) {
+          ratio = ratioWidth;
+        } else {
+          ratio = ratioHeight;
+        }
+      }
+      //Scale
+      AffineTransform at = new AffineTransform();
+      at.scale(ratio, ratio);
+      AffineTransformOp op = new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+      BufferedImage scaledImage = op.filter(originalImage, null);
+
+      //Rotate
+      at = new AffineTransform();
+      at.setToTranslation(scaledImage.getHeight() / 2, scaledImage.getWidth() / 2);
+      at.rotate(Math.toRadians(90));
+      at.translate(-(scaledImage.getWidth() / 2), -(scaledImage.getHeight() / 2));
+      op = new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+      BufferedImage thumbnailImage = op.filter(scaledImage, null);
+
+      File thumbnailFile = new File(filename);
+      ImageIO.write(thumbnailImage, "jpg", thumbnailFile);
+      return (process.waitFor());
+    } catch (Exception e) {
+      e.printStackTrace(System.out);
+      System.err.println("GraphicUtils-> urlToJpeg error: " + e.toString());
+      return (1);
+    }
+  }
+
+
+  /**
+   *  Uses HtmlDoc command line tool, pipes an HTML URL to GhostScript
+   *  which outputs a JPEG file.
+   *
+   *@param  url       Description of the Parameter
+   *@param  filename  Description of the Parameter
+   *@return           Description of the Return Value
+   */
+  public static int urlToJpegThumbnail(String url, String filename) {
+    Process process;
+    Runtime runtime;
+    java.io.InputStream input;
+    byte buffer[];
+    int bytes;
+    String command[] = {"/bin/sh", "-c", "/usr/bin/htmldoc --quiet --jpeg " +
+        "--webpage -t ps --left 0 --top 0 " +
+        "--header ... --footer ... " + url + " " +
+        "| /usr/bin/gs -q -sDEVICE=jpeg -dNOPAUSE -dBATCH -sOutputFile=- -"};
+    runtime = Runtime.getRuntime();
+    try {
+      process = runtime.exec(command);
+      input = process.getInputStream();
+      BufferedImage originalImage = ImageIO.read(input);
+      File thumbnailFile = new File(filename);
+      ImageIO.write(originalImage, "jpg", thumbnailFile);
+      return (0);
+    } catch (Exception e) {
+      e.printStackTrace(System.out);
+      System.err.println("GraphicUtils-> urlToJpeg error: " + e.toString());
+      return (1);
+    }
+  }
 }
 
