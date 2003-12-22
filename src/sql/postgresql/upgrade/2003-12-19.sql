5cefe12f94af6cb55139ae9d8b8e5b2fd4a69d8f
mrajkowski <mrajkowski@concursive.com>
2003-12-22 14:10:35 +0000

    FIX: Finalized for Help Desk

--- src/sql/postgresql/upgrade/2003-12-19.sql
+++ src/sql/postgresql/upgrade/2003-12-19.sql
@@ -1,5 +1,5 @@
 /* Renaming the module so permissions need to be updated too */
 UPDATE permission_category SET category = 'Help Desk' WHERE category = 'Tickets';
 UPDATE permission SET description = 'Access to Help Desk module' WHERE permission = 'tickets';
-UPDATE module_field_categorylink SET description = 'Help Desk' WHERE description = 'Tickets';
+UPDATE module_field_categorylink SET description = 'Tickets' WHERE description = 'Help Desk';
 
