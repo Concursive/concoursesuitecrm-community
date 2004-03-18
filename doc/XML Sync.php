a6ab1a6f7fc6bbe034953f882a3657dba2824f91
mrajkowski <mrajkowski@concursive.com>
2004-03-18 14:35:12 +0000

    CHANGE: Update for Dark Horse CRM 2.7 TODO: Add more mappings

--- doc/XML Sync.php
+++ doc/XML Sync.php
@@ -1,21 +1,22 @@
 <?
- //Site to post to
- $server = "ebizteam.aspcfs.com"
+ // Site to post to
+ $server = "www.your_darkhorse_crm_server.com"
  $path = "/ProcessPacket.do"
- //Begin the transfer
+ // Begin the transfer
  $socket = fsockopen($server, 80);
  if (!$socket) {
-   //error: could not connect to server
-   //return
+   // error: could not connect to server
+   // return
  } else {
+   // Post the XML document
    $request = "POST " . $path . " HTTP/1.0\r\n" .
      "Host: " . $server . "\r\n" .
      "Content-Type: text/xml\r\n" .
      "Content-Length: " . strlen($xmlDocument) . "\r\n\r\n" .
      $xmlDocument;
    if (!fputs($socket, $request, strlen($request))) {
-     //error: could not write to server
-     //return
+     // error: could not write to server
+     // return
    }
    $response = '';
    while ($data = fread($socket, 32768)) {
@@ -23,8 +24,10 @@
    }
    fclose($socket);
    if ($response = '') {
-     //error: no response from server
-     //return
+     // error: no response from server
+     // return
+   } else {
+     // review the response for status
    }
  }
 ?>
