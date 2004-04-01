026abb2129381b589093571964ebcfa771221a25
mrajkowski <mrajkowski@concursive.com>
2004-04-01 14:15:12 +0000

    MERGE: from f-20040317-dataline NEW: Function to check real numbers

--- src/web/javascript/checkNumber.js
+++ src/web/javascript/checkNumber.js
@@ -1,6 +1,5 @@
 function checkNumber(number) {
 	var validNumbers = "0123456789.";
-	
 	if(number.length > 0){
     for(i = 0; i < number.length; i++){
       if(validNumbers.indexOf(number.charAt(i)) == -1){
@@ -10,4 +9,17 @@ function checkNumber(number) {
   }
   return true;
 }
-  
+
+
+function checkRealNumber(number) {
+	var validNumbers = "-0123456789.";
+	if(number.length > 0){
+    for(i = 0; i < number.length; i++){
+      if(validNumbers.indexOf(number.charAt(i)) == -1){
+        return false
+      }
+    }
+  }
+  return true;
+}
+
