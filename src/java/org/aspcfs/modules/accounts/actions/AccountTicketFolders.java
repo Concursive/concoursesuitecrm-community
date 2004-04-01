fd29bfec0a1519a7d257158f9a80307d6ec59023
mrajkowski <mrajkowski@concursive.com>
2004-04-01 14:34:54 +0000

    MERGE: from f-20040317-dataline FIX: PagedListInfo name was incorrectly re-used from another module

--- src/java/org/aspcfs/modules/accounts/actions/AccountTicketFolders.java
+++ src/java/org/aspcfs/modules/accounts/actions/AccountTicketFolders.java
@@ -81,7 +81,7 @@ public final class AccountTicketFolders extends CFSModule {
         if (recordId == null && thisCategory.getAllowMultipleRecords()) {
           //The user didn't request a specific record, so show a list
           //of records matching this category that the user can choose from
-          PagedListInfo folderListInfo = this.getPagedListInfo(context, "AccountFolderInfo");
+          PagedListInfo folderListInfo = this.getPagedListInfo(context, "AccountTicketsFolderInfo");
           folderListInfo.setLink("TroubleTicketsFolders.do?command=Fields&ticketId=" + ticketId + "&catId=" + selectedCatId);
 
           CustomFieldRecordList recordList = new CustomFieldRecordList();
