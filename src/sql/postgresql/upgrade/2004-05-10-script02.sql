61bec911c79ffd31c75e87f317a5e60f8fada7cc
mrajkowski <mrajkowski@concursive.com>
2004-05-17 12:44:02 +0000

    MERGE: from f-dataline_20040507 NEW: database and report upgrade scripts

---
+++ src/sql/postgresql/upgrade/2004-05-10-script02.sql
@@ -0,0 +1,9 @@
+CREATE TABLE service_contract_products(
+  id INT SERIAL PRIMARY KEY,
+  link_contract_id INT REFERENCES service_contract(contract_id),
+  link_product_id INT REFERENCES product_catalog(product_id),
+);
+
+/* assuming upgrade scripts to add product tables are in place*/ 
+INSERT INTO  product_category(category_name, enteredby,modifiedby) 
+VALUES ('Labor Category',0,0);
