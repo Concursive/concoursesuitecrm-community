9b510ffd6a573e1f614874ff853de04615858ca8
mrajkowski <mrajkowski@concursive.com>
2004-05-27 13:49:18 +0000

    NEW: quote fields and quote notes table

---
+++ src/sql/mssql/new_quote_adjustment.sql
@@ -0,0 +1,19 @@
+/*
+  New columns to accomadate product and customer_product in quotes
+*/
+ALTER TABLE quote_entry ADD product_id INTEGER REFERENCES product_catalog(product_id);
+ALTER TABLE quote_entry ADD customer_product_id INTEGER REFERENCES customer_product(customer_product_id);
+
+-- Each quote has notes
+-- Example: Notes entered by Advertiser, Notes entered by Designer
+CREATE TABLE quote_notes (
+  notes_id INT IDENTITY PRIMARY KEY,
+  quote_id INTEGER REFERENCES quote_entry(quote_id),
+  notes TEXT,
+  -- record status
+  enteredby INTEGER NOT NULL REFERENCES access(user_id),
+  entered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modifiedby INTEGER NOT NULL REFERENCES access(user_id),
+  modified DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
