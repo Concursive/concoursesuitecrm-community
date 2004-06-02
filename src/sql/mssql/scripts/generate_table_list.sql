13f52e26344a6333aa29df353e8534dab413af2b
mrajkowski <mrajkowski@concursive.com>
2004-06-02 16:57:32 +0000

    FIX: column names are required for identity inserts

--- src/sql/mssql/scripts/generate_table_list.sql
+++ src/sql/mssql/scripts/generate_table_list.sql
@@ -2,7 +2,7 @@ SELECT 'EXEC sp_generate_inserts ' +
 '[' + name + ']' + 
 ',@owner = ' + 
 '[' + RTRIM(USER_NAME(uid)) + '],' + 
-'@ommit_images = 1, @disable_constraints = 0, @include_column_list = 0'
+'@ommit_images = 1, @disable_constraints = 0'
 FROM sysobjects 
 WHERE type = 'U' AND 
 OBJECTPROPERTY(id,'ismsshipped') = 0
