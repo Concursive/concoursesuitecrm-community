0629ef0767e9f2293b4f8e27922abe8f561b4ddc
mrajkowski <mrajkowski@concursive.com>
2004-06-03 16:01:43 +0000

    MERGE: null fixes

--- src/web/javascript/checkRadioButton.js
+++ src/web/javascript/checkRadioButton.js
@@ -1,29 +1,57 @@
 function getSelectedRadio(buttonGroup) {
-   // returns the array number of the selected radio button or -1 if no button is selected
-   if (buttonGroup[0]) { // if the button group is an array (one button is not an array)
-      for (var i=0; i<buttonGroup.length; i++) {
-         if (buttonGroup[i].checked) {
-            return i
-         }
-      }
-   } else {
-      if (buttonGroup.checked) { return 0; } // if the one button is checked, return zero
-   }
-   // if we get to this point, no radio button is selected
-   return -1;
+  if (!buttonGroup) {
+    // there is no button group
+    return -1;
+  }
+  // returns the array number of the selected radio button or -1 if no button is selected
+  if (buttonGroup[0]) { // if the button group is an array (one button is not an array)
+     for (var i=0; i<buttonGroup.length; i++) {
+        if (buttonGroup[i].checked) {
+           return i
+        }
+     }
+  } else {
+     if (buttonGroup.checked) { return 0; } // if the one button is checked, return zero
+  }
+  // if we get to this point, no radio button is selected
+  return -1;
 } // Ends the "getSelectedRadio" function
 
 function getSelectedRadioValue(buttonGroup) {
-   // returns the value of the selected radio button or "" if no button is selected
-   var i = getSelectedRadio(buttonGroup);
-   if (i == -1) {
-      return "";
-   } else {
-      if (buttonGroup[i]) { // Make sure the button group is an array (not just one button)
-         return buttonGroup[i].value;
-      } else { // The button group is just the one button, and it is checked
-         return buttonGroup.value;
-      }
-   }
+  if (!buttonGroup) {
+    // there is no button group
+    return "";
+  }
+  // returns the value of the selected radio button or "" if no button is selected
+  var i = getSelectedRadio(buttonGroup);
+  if (i == -1) {
+     return "";
+  } else {
+     if (buttonGroup[i]) { // Make sure the button group is an array (not just one button)
+        return buttonGroup[i].value;
+     } else { // The button group is just the one button, and it is checked
+        return buttonGroup.value;
+     }
+  }
 } // Ends the "getSelectedRadioValue" function
 
+function setSelectedRadio(buttonGroup, setValue) {
+  if (!buttonGroup) {
+    // there is no button group
+    return -1;
+  }
+  // returns the array number of the selected radio button or -1 if no button is selected
+  if (buttonGroup[0]) { // if the button group is an array (one button is not an array)
+     for (var i=0; i<buttonGroup.length; i++) {
+        if (buttonGroup[i].value == setValue) {
+           buttonGroup[i].checked = true;
+           break;
+        }
+     }
+     return 0;
+  } else {
+     if (buttonGroup.value == setValue) { buttonGroup.checked = true; return 0 } // if the one button is checked, return zero
+  }
+  // if we get to this point, no radio button is selected
+  return -1;
+}
