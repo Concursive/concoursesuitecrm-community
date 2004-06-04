169bed6aa4fc92ac97345a2822d2619ea51f28a2
mrajkowski <mrajkowski@concursive.com>
2004-06-04 13:42:16 +0000

    CHANGE: 2.8 beta 2

--- src/java/org/aspcfs/modules/system/base/ApplicationVersion.java
+++ src/java/org/aspcfs/modules/system/base/ApplicationVersion.java
@@ -8,6 +8,6 @@ package org.aspcfs.modules.system.base;
  *@version    $Id$
  */
 public class ApplicationVersion {
-  public final static String VERSION = "2.8 beta (2004-05-04)";
+  public final static String VERSION = "2.8 beta 2 (2004-06-03)";
 }
 
