8a793b8da92edd3171022f5bc64080f36b7aafe6
mrajkowski <mrajkowski@concursive.com>
2004-06-04 13:19:39 +0000

    FIX: broken sql script

--- src/sql/mssql/upgrade/2004-05-10-script02.sql
+++ src/sql/mssql/upgrade/2004-05-10-script02.sql
@@ -1,7 +1,7 @@
 CREATE TABLE service_contract_products(
   id INT IDENTITY PRIMARY KEY,
   link_contract_id INT REFERENCES service_contract(contract_id),
-  link_product_id INT REFERENCES product_catalog(product_id),
+  link_product_id INT REFERENCES product_catalog(product_id)
 );
 
 /* assuming upgrade scripts to add product tables are in place*/ 
