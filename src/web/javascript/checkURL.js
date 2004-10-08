72e158b4e5cb427aa0daa82d3b753217163fad2a
mrajkowski <mrajkowski@concursive.com>
2004-10-08 14:38:39 +0000

    MERGE: from bug-version29_20041001 FIX: Complex URLs previously did not work

--- src/web/javascript/checkURL.js
+++ src/web/javascript/checkURL.js
@@ -3,16 +3,14 @@
  * @arg1 = url to check
  */
 function checkURL(url){
- if (url.length > 0){
-   url = padURL(url);
-   var j = new RegExp(); 
-   topLevelCount = url.length - (url.lastIndexOf('.') + 1);
-   j.compile("^[A-Za-z]+://[A-Za-z0-9-]+\.[A-Za-z]{2}");
-   if (!j.test(url) || (url.indexOf('.', 0) == -1) ||  topLevelCount > 4 || topLevelCount < 2) {
-     return false;
-   }
- }
- return true;
+  if (url.length > 0){
+    url = padURL(url);
+    var i = url.search(/^[A-Za-z]+:\/\/\w+([\.-]?\w+)*/);
+    if (i == -1  || (url.indexOf('.', 0) == -1)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function padURL(url){
