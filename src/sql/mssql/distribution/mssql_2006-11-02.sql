58a00a9900d4c3454ee073bf0774689f0d6043db
matt <mrajkowski@concursive.com>
2006-11-13 21:12:59 +0000

    FIX: sql server 2000 needs drop constraint

--- src/sql/mssql/distribution/mssql_2006-11-02.sql
+++ src/sql/mssql/distribution/mssql_2006-11-02.sql
@@ -77,8 +77,47 @@ UPDATE call_log
  SET followup_contact_id = contact_id
  WHERE followup_contact_id IS NULL AND contact_id IS NOT NULL;
 
+EXEC sp_executesql N'
+DECLARE @sql nvarchar(4000)
+DECLARE CNSTR CURSOR LOCAL FAST_FORWARD READ_ONLY FOR
+     SELECT ''ALTER TABLE ''+@table+'' DROP CONSTRAINT ''+QUOTENAME(o.name)
+     FROM sysobjects o
+     INNER JOIN sysconstraints sc ON o.id = sc.constid
+     WHERE object_name(o.parent_obj) = ''permission_category''
+     AND o.xtype = ''D''
+     AND o.name LIKE ''DF__permissio__produ__%''
+OPEN CNSTR
+FETCH CNSTR INTO @sql
+WHILE @@FETCH_STATUS=0
+BEGIN
+    EXEC sp_executesql @sql
+    FETCH CNSTR INTO @sql
+END
+CLOSE CNSTR DEALLOCATE CNSTR',N'@table nvarchar(261)','[permission_category]'
+GO
+
 alter table permission_category
  drop column products;
+
+
+EXEC sp_executesql N'
+DECLARE @sql nvarchar(4000)
+DECLARE CNSTR CURSOR LOCAL FAST_FORWARD READ_ONLY FOR
+     SELECT ''ALTER TABLE ''+@table+'' DROP CONSTRAINT ''+QUOTENAME(o.name)
+     FROM sysobjects o
+     INNER JOIN sysconstraints sc ON o.id = sc.constid
+     WHERE object_name(o.parent_obj) = ''permission_category''
+     AND o.xtype = ''D''
+     AND o.name LIKE ''DF__permissio__impor__%''
+OPEN CNSTR
+FETCH CNSTR INTO @sql
+WHILE @@FETCH_STATUS=0
+BEGIN
+    EXEC sp_executesql @sql
+    FETCH CNSTR INTO @sql
+END
+CLOSE CNSTR DEALLOCATE CNSTR',N'@table nvarchar(261)','[permission_category]'
+GO
 
 alter table permission_category
  drop column importer;
