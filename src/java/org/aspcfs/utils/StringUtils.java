322e3359f848be4acdf22f6581d4e52424369486
matt <mrajkowski@concursive.com>
2006-11-13 15:43:59 +0000

    FIX: a new line character was being added even if the file did not have another line

--- src/java/org/aspcfs/utils/StringUtils.java
+++ src/java/org/aspcfs/utils/StringUtils.java
@@ -554,9 +554,13 @@ public class StringUtils {
     StringBuffer text = new StringBuffer();
     BufferedReader in = new BufferedReader(new FileReader(filename));
     String line = null;
+    boolean hasLine = false;
     while ((line = in.readLine()) != null) {
+      if (hasLine) {
+        text.append(ls);
+      }
       text.append(line);
-      text.append(ls);
+      hasLine = true;
     }
     in.close();
     return text.toString();
