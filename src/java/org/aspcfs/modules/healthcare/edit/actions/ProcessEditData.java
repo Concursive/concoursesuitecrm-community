9745f9048b69295f36bb1428f355cf864961ca37
srinivasar@cybage.com <srinivasar@cybage.com>
2007-12-09 11:59:02 +0000

    Ticket # 754: license information updating

--- src/java/org/aspcfs/modules/healthcare/edit/actions/ProcessEditData.java
+++ src/java/org/aspcfs/modules/healthcare/edit/actions/ProcessEditData.java
@@ -1,15 +1,15 @@
 /*
- *  Copyright(c) 2004 Dark Horse Ventures LLC (http://www.centriccrm.com/) All
+ *  Copyright(c) 2004 Concursive Corporation (http://www.concursive.com/) All
  *  rights reserved. This material cannot be distributed without written
- *  permission from Dark Horse Ventures LLC. Permission to use, copy, and modify
+ *  permission from Concursive Corporation. Permission to use, copy, and modify
  *  this material for internal use is hereby granted, provided that the above
- *  copyright notice and this permission notice appear in all copies. DARK HORSE
- *  VENTURES LLC MAKES NO REPRESENTATIONS AND EXTENDS NO WARRANTIES, EXPRESS OR
+ *  copyright notice and this permission notice appear in all copies. CONCURSIVE
+ *  CORPORATION MAKES NO REPRESENTATIONS AND EXTENDS NO WARRANTIES, EXPRESS OR
  *  IMPLIED, WITH RESPECT TO THE SOFTWARE, INCLUDING, BUT NOT LIMITED TO, THE
  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ANY PARTICULAR
  *  PURPOSE, AND THE WARRANTY AGAINST INFRINGEMENT OF PATENTS OR OTHER
  *  INTELLECTUAL PROPERTY RIGHTS. THE SOFTWARE IS PROVIDED "AS IS", AND IN NO
- *  EVENT SHALL DARK HORSE VENTURES LLC OR ANY OF ITS AFFILIATES BE LIABLE FOR
+ *  EVENT SHALL CONCURSIVE CORPORATION OR ANY OF ITS AFFILIATES BE LIABLE FOR
  *  ANY DAMAGES, INCLUDING ANY LOST PROFITS OR OTHER INCIDENTAL OR CONSEQUENTIAL
  *  DAMAGES RELATING TO THE SOFTWARE.
  */
@@ -308,7 +308,7 @@ public final class ProcessEditData extends CFSModule {
                       notFoundIds.add(t);
                     }
                     testBuffer.append(
-                        "Skipping ID " + t + "- could not locate in Centric CRM!\n");
+                        "Skipping ID " + t + "- could not locate in Concourse Suite Community Edition!\n");
                   }
                 } else if (sub.indexOf("TOTAL TRANSACTIONS") > -1) {
                   break;
@@ -420,7 +420,7 @@ public final class ProcessEditData extends CFSModule {
           totalsCount + " office transaction records (totals) were successfully inserted.\n\n");
       if (notFoundIds.size() > 0) {
         testBuffer.append(
-            "The following " + notFoundIds.size() + " provider IDs were not found in Centric CRM:\n\n");
+            "The following " + notFoundIds.size() + " provider IDs were not found in Concourse Suite Community Edition:\n\n");
         for (int z = 0; z < notFoundIds.size(); z++) {
           testBuffer.append(notFoundIds.get(z) + "\n");
         }
