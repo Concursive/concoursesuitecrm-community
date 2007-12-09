8d492d1a6ee5ad7808829d2659fc93f1360a2468
srinivasar@cybage.com <srinivasar@cybage.com>
2007-12-09 12:21:07 +0000

    Ticket # 754: license information updating

--- src/java/org/aspcfs/modules/pipeline/actions/LeadsQuotes.java
+++ src/java/org/aspcfs/modules/pipeline/actions/LeadsQuotes.java
@@ -1,15 +1,15 @@
 /*
- *  Copyright(c) 2004 Dark Horse Ventures LLC (http://www.centriccrm.com/) All
+ *  Copyright(c) 2004 Concursive Corporation (http://www.concursive.com/) All
  *  rights reserved. This material cannot be distributed without written
- *  permission from Dark Horse Ventures LLC. Permission to use, copy, and modify
+ *  permission from Concursive Corporation. Permission to use, copy, and modify
  *  this material for internal use is hereby granted, provided that the above
- *  copyright notice and this permission notice appear in all copies. DARK HORSE
- *  VENTURES LLC MAKES NO REPRESENTATIONS AND EXTENDS NO WARRANTIES, EXPRESS OR
+ *  copyright notice and this permission notice appear in all copies. CONCURSIVE
+ *  CORPORATION MAKES NO REPRESENTATIONS AND EXTENDS NO WARRANTIES, EXPRESS OR
  *  IMPLIED, WITH RESPECT TO THE SOFTWARE, INCLUDING, BUT NOT LIMITED TO, THE
  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ANY PARTICULAR
  *  PURPOSE, AND THE WARRANTY AGAINST INFRINGEMENT OF PATENTS OR OTHER
  *  INTELLECTUAL PROPERTY RIGHTS. THE SOFTWARE IS PROVIDED "AS IS", AND IN NO
- *  EVENT SHALL DARK HORSE VENTURES LLC OR ANY OF ITS AFFILIATES BE LIABLE FOR
+ *  EVENT SHALL CONCURSIVE CORPORATION OR ANY OF ITS AFFILIATES BE LIABLE FOR
  *  ANY DAMAGES, INCLUDING ANY LOST PROFITS OR OTHER INCIDENTAL OR CONSEQUENTIAL
  *  DAMAGES RELATING TO THE SOFTWARE.
  */
@@ -551,7 +551,7 @@ public final class LeadsQuotes extends CFSModule {
       quote.setBuildTicket(true);
       quote.queryRecord(db, Integer.parseInt(quoteId));
 
-      htmlDialog.setTitle("Centric CRM: Quote Management");
+      htmlDialog.setTitle("Concourse Suite Community Edition: Quote Management");
       DependencyList dependencies = quote.processDependencies(db);
       dependencies.setSystemStatus(systemStatus);
       htmlDialog.addMessage(
