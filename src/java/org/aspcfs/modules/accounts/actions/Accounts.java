527423271232328b6eb786eed6ca3c0843da809e
srinivasar@cybage.com <srinivasar@cybage.com>
2007-12-09 11:06:34 +0000

    Ticket # 754: license information updating

--- src/java/org/aspcfs/modules/accounts/actions/Accounts.java
+++ src/java/org/aspcfs/modules/accounts/actions/Accounts.java
@@ -1,15 +1,15 @@
 /*
- *  Copyright(c) 2004 Dark Horse Ventures LLC (http://www.centriccrm.com/) All
+ *  Copyright(c) 2004 Concursive Corporation (http://www.concursive.com/) All
  *  rights reserved. This material cannot be distributed without written
- *  permission from Dark Horse Ventures LLC. Permission to use, copy, and modify
+ *  permission from Concursive Corporation. Permission to use, copy, and modify
  *  this material for internal use is hereby granted, provided that the above
- *  copyright notice and this permission notice appear in all copies. DARK HORSE
- *  VENTURES LLC MAKES NO REPRESENTATIONS AND EXTENDS NO WARRANTIES, EXPRESS OR
+ *  copyright notice and this permission notice appear in all copies. CONCURSIVE
+ *  CORPORATION MAKES NO REPRESENTATIONS AND EXTENDS NO WARRANTIES, EXPRESS OR
  *  IMPLIED, WITH RESPECT TO THE SOFTWARE, INCLUDING, BUT NOT LIMITED TO, THE
  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ANY PARTICULAR
  *  PURPOSE, AND THE WARRANTY AGAINST INFRINGEMENT OF PATENTS OR OTHER
  *  INTELLECTUAL PROPERTY RIGHTS. THE SOFTWARE IS PROVIDED "AS IS", AND IN NO
- *  EVENT SHALL DARK HORSE VENTURES LLC OR ANY OF ITS AFFILIATES BE LIABLE FOR
+ *  EVENT SHALL CONCURSIVE CORPORATION OR ANY OF ITS AFFILIATES BE LIABLE FOR
  *  ANY DAMAGES, INCLUDING ANY LOST PROFITS OR OTHER INCIDENTAL OR CONSEQUENTIAL
  *  DAMAGES RELATING TO THE SOFTWARE.
  */
@@ -292,7 +292,7 @@ public final class Accounts extends CFSModule {
       //Accounts with opportunities report
       if (type.equals("5")) {
         OpportunityReport oppReport = new OpportunityReport();
-        oppReport.setHeader(systemStatus.getLabel("Centric CRM Accounts", "centriccrm.accounts"));
+        oppReport.setHeader(systemStatus.getLabel("Concourse Suite Community Edition Accounts", "centriccrm.accounts"));
         oppReport.setFilePath(filePath);
         oppReport.setEnteredBy(getUserId(context));
         oppReport.setModifiedBy(getUserId(context));
@@ -321,7 +321,7 @@ public final class Accounts extends CFSModule {
         contactReport.setEnteredBy(getUserId(context));
         contactReport.setModifiedBy(getUserId(context));
         contactReport.setSubject(subject);
-        contactReport.setHeader(systemStatus.getLabel("Centric CRM Accounts", "centriccrm.accounts"));
+        contactReport.setHeader(systemStatus.getLabel("Concourse Suite Community Edition Accounts", "centriccrm.accounts"));
         contactReport.addIgnoreTypeId(Contact.EMPLOYEE_TYPE);
         contactReport.addIgnoreTypeId(Contact.LEAD_TYPE);
         contactReport.setSiteId(this.getUserSiteId(context));
@@ -352,7 +352,7 @@ public final class Accounts extends CFSModule {
         if (ticReport.getSiteId() == -1) {
           ticReport.setIncludeAllSites(true);
         }
-        ticReport.setHeader(systemStatus.getLabel("Centric CRM Accounts", "centriccrm.accounts"));
+        ticReport.setHeader(systemStatus.getLabel("Concourse Suite Community Edition Accounts", "centriccrm.accounts"));
         ticReport.setJoinOrgs(true);
         if (ownerCriteria.equals("my")) {
           ticReport.setLimitId(this.getUserId(context));
